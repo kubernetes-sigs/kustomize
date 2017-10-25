96496337bd04b93dc270bfddb25d2695b82e3f90
abkaskari <abdelhak99jaafari@gmail.com>
2017-10-25 10:29:36 -0700

    tweakprecommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 if [ -n "$failIt" ]; then
-	echo "Expecting failIt to be empty."
+  echo "Expecting failIt to be empty."
   exit 1
 fi
 
-tmp=$(gofmt -s -d -l . 2>&1 )
-if [ -n "$tmp" ]; then
-		printf >&2 'gofmt failed for:\n%s\n' "$tmp"
-		failIt=1
+wantEmpty=$(gofmt -s -d -l . 2>&1 )
+if [ -n "$wantEmpty" ]; then
+  printf >&2 'gofmt failed for:\n%s\n' "$wantEmpty"
+  failIt=1
 fi
 
-tmp=$(goimports -l .)
-if [ -n "$tmp" ]; then
-		printf >&2 'goimports failed for:\n%s\n' "$tmp"
-		failIt=1
+wantEmpty=$(goimports -l .)
+if [ -n "$wantEmpty" ]; then
+  printf >&2 'goimports failed for:\n%s\n' "$wantEmpty"
+  failIt=1
 fi
 
-tmp=$(go vet -all ./... 2>&1)
-if [ -n "$tmp" ]; then
-		printf >&2 'govet failed for:\n%s\n' "$tmp"
-		failIt=1
+wantEmpty=$(go vet -all ./... 2>&1)
+if [ -n "$wantEmpty" ]; then
+  printf >&2 'govet failed for:\n%s\n' "$wantEmpty"
+  failIt=1
 fi
 
-tmp=$(golint ./...)
-if [ -n "$tmp" ]; then
-		printf >&2 'golint failed for:\n%s\n' "$tmp"
-		failIt=1
+wantEmpty=$(golint ./...)
+if [ -n "$wantEmpty" ]; then
+  printf >&2 'golint failed for:\n%s\n' "$wantEmpty"
+  failIt=1
 fi
 
 if [ -n "$failIt" ]; then
-	unset failIt
+  unset failIt
   exit 1
 fi
 
