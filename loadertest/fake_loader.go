f640a250d99f89fbc605eb4efcf86d1145f89771
Sean Sullivan <seans@google.com>
2018-02-28 11:41:18 -0800

    Simplified Loader.New() to always take a directory (not a file)

--- loadertest/fake_loader.go
+++ loadertest/fake_loader.go
@@ -18,7 +18,6 @@ package loadertest
 
 import (
 	"os"
-	"strings"
 
 	"k8s.io/kubectl/pkg/kinflate/util/fs"
 	"k8s.io/kubectl/pkg/loader"
@@ -31,19 +30,15 @@ type FakeLoader struct {
 }
 
 // NewFakeLoader returns a Loader that delegates calls, and encapsulates
-// a fake file system that the Loader reads from. "initialFilePath" parameter
-// must be an absolute path, and it can be a directory if it has a
-// trailing slash: "/home/seans/project/" (dir) -- "/home/seans/project" (file)
-func NewFakeLoader(initialFilePath string) FakeLoader {
+// a fake file system that the Loader reads from. "initialDir" parameter
+// must be an full, absolute directory (trailing slash doesn't matter).
+func NewFakeLoader(initialDir string) FakeLoader {
 	// Create fake filesystem and inject it into initial Loader.
 	fakefs := fs.MakeFakeFS()
-	if strings.HasSuffix(initialFilePath, "/") {
-		fakefs.Mkdir(initialFilePath, 0x777)
-	}
 	var schemes []loader.SchemeLoader
 	schemes = append(schemes, loader.NewFileLoader(fakefs))
 	rootLoader := loader.Init(schemes)
-	loader, _ := rootLoader.New(initialFilePath)
+	loader, _ := rootLoader.New(initialDir)
 	return FakeLoader{fs: fakefs, delegate: loader}
 }
 
