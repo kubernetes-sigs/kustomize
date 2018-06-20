48831716d7d06a69317074ad61359d2eac543a35
abkaskari <abdelhak99jaafari@gmail.com>
2018-06-19 17:00:25 -0700

    Implements labels and annotations as subcommands of edit

--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -96,6 +96,15 @@ func newCmdAdd(fsys fs.FileSystem) *cobra.Command {
 	# Adds one or more base directories to the kustomization
 	kustomize edit add base <filepath>
 	kustomize edit add base <filepath1>,<filepath2>,<filepath3>
+<<<<<<< HEAD
+=======
+
+	# Adds one or more commonLabels to the kustomization
+	kustomize edit add label {labelKey1:labelValue1},{labelKey2:labelValue2}
+
+	# Adds one or more commonAnnotations to the kustomization
+	kustomize edit add annotation {annotationKey1:annotationValue1},{annotationKey2:annotationValue2}
+>>>>>>> Implements labels and annotations as subcommands of edit
 `,
 		Args: cobra.MinimumNArgs(1),
 	}
@@ -104,6 +113,8 @@ func newCmdAdd(fsys fs.FileSystem) *cobra.Command {
 		newCmdAddPatch(fsys),
 		newCmdAddConfigMap(fsys),
 		newCmdAddBase(fsys),
+		newCmdAddLabel(fsys),
+		newCmdAddAnnotation(fsys),
 	)
 	return c
 }
