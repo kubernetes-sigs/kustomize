a8a1d2a0fd46f002b653e7c1f35c024df0379f4d
guineveresaenger <guineveresaenger@gmail.com>
2018-07-16 13:57:28 -0700

    Uses single file for both addLabel and addAnnotation commands, as the code is nearly identical. Tests included.

--- pkg/commands/addannotation.go
+++ pkg/commands/addannotation.go
@@ -17,24 +17,24 @@ limitations under the License.
 package commands
 
 import (
-	"errors"
-	"fmt"
-	"regexp"
-	"strings"
+	// "errors"
+	// "fmt"
+	// "regexp"
+	// "strings"
 
 	"github.com/spf13/cobra"
 
-	"github.com/kubernetes-sigs/kustomize/pkg/constants"
+	// "github.com/kubernetes-sigs/kustomize/pkg/constants"
 	"github.com/kubernetes-sigs/kustomize/pkg/fs"
 )
 
-type addAnnotationOptions struct {
-	annotations string
-}
+// type addAnnotationOptions struct {
+// 	annotations string
+// }
 
 // newCmdAddAnnotation adds one or more commonAnnotations to the kustomization file.
 func newCmdAddAnnotation(fsys fs.FileSystem) *cobra.Command {
-	var o addAnnotationOptions
+	var o addMetadataOptions
 
 	cmd := &cobra.Command{
 		Use:   "annotation",
@@ -42,7 +42,8 @@ func newCmdAddAnnotation(fsys fs.FileSystem) *cobra.Command {
 		Example: `
 		add annotation {annotationKey1:annotationValue1},{annotationKey2:annotationValue2}`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := o.Validate(args)
+			mdKind := "annotation"
+			err := o.Validate(args, mdKind)
 			if err != nil {
 				return err
 			}
@@ -50,62 +51,62 @@ func newCmdAddAnnotation(fsys fs.FileSystem) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			return o.RunAddAnnotation(fsys)
+			return o.RunAddMetadata(fsys, mdKind)
 		},
 	}
 	return cmd
 }
 
-// Validate validates addAnnotation command.
-func (o *addAnnotationOptions) Validate(args []string) error {
-	if len(args) < 1 {
-		return errors.New("must specify an annotation")
-	}
-	if len(args) > 1 {
-		return errors.New("annotations must be comma-separated, with no spaces. See help text for example.")
-	}
-	inputs := strings.Split(args[0], ",")
-	for _, input := range inputs {
-		ok, err := regexp.MatchString(`\A([a-zA-Z0-9_.-]+):([a-zA-Z0-9_.-]+)\z`, input)
-		if err != nil {
-			return err
-		}
-		if !ok {
-			return fmt.Errorf("invalid annotation format: %s", input)
-		}
-	}
-	o.annotations = args[0]
-	return nil
-}
+// // Validate validates addAnnotation command.
+// func (o *addAnnotationOptions) Validate(args []string) error {
+// 	if len(args) < 1 {
+// 		return errors.New("must specify an annotation")
+// 	}
+// 	if len(args) > 1 {
+// 		return errors.New("annotations must be comma-separated, with no spaces. See help text for example.")
+// 	}
+// 	inputs := strings.Split(args[0], ",")
+// 	for _, input := range inputs {
+// 		ok, err := regexp.MatchString(`\A([a-zA-Z0-9_.-]+):([a-zA-Z0-9_.-]+)\z`, input)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if !ok {
+// 			return fmt.Errorf("invalid annotation format: %s", input)
+// 		}
+// 	}
+// 	o.annotations = args[0]
+// 	return nil
+// }
 
-// Complete completes addAnnotation command.
-func (o *addAnnotationOptions) Complete(cmd *cobra.Command, args []string) error {
-	return nil
-}
+// // Complete completes addAnnotation command.
+// func (o *addAnnotationOptions) Complete(cmd *cobra.Command, args []string) error {
+// 	return nil
+// }
 
-// RunAddAnnotation runs addAnnotation command (do real work).
-func (o *addAnnotationOptions) RunAddAnnotation(fsys fs.FileSystem) error {
-	mf, err := newKustomizationFile(constants.KustomizationFileName, fsys)
-	if err != nil {
-		return err
-	}
+// // RunAddAnnotation runs addAnnotation command (do real work).
+// func (o *addAnnotationOptions) RunAddAnnotation(fsys fs.FileSystem) error {
+// 	mf, err := newKustomizationFile(constants.KustomizationFileName, fsys)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	m, err := mf.read()
-	if err != nil {
-		return err
-	}
+// 	m, err := mf.read()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if m.CommonAnnotations == nil {
-		m.CommonAnnotations = make(map[string]string)
-	}
-	annotations := strings.Split(o.annotations, ",")
-	for _, ann := range annotations {
-		kv := strings.Split(ann, ":")
-		if _, ok := m.CommonAnnotations[kv[0]]; ok {
-			return fmt.Errorf("annotation %s already in kustomization file", kv[0])
-		}
-		m.CommonAnnotations[kv[0]] = kv[1]
-	}
+// 	if m.CommonAnnotations == nil {
+// 		m.CommonAnnotations = make(map[string]string)
+// 	}
+// 	annotations := strings.Split(o.annotations, ",")
+// 	for _, ann := range annotations {
+// 		kv := strings.Split(ann, ":")
+// 		if _, ok := m.CommonAnnotations[kv[0]]; ok {
+// 			return fmt.Errorf("annotation %s already in kustomization file", kv[0])
+// 		}
+// 		m.CommonAnnotations[kv[0]] = kv[1]
+// 	}
 
-	return mf.write(m)
-}
+// 	return mf.write(m)
+// }
