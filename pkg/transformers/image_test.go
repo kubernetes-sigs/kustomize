c1f318dd7eaa2a77bdfaa73ba7103e1f686c3c91
Nestor <nesterran@gmail.com>
2019-01-16 15:10:45 +0100

    Add image transformer

--- pkg/transformers/image_test.go
+++ pkg/transformers/image_test.go
@@ -22,13 +22,13 @@ import (
 
 	"sigs.k8s.io/kustomize/k8sdeps/kunstruct"
 	"sigs.k8s.io/kustomize/pkg/gvk"
+	"sigs.k8s.io/kustomize/pkg/image"
 	"sigs.k8s.io/kustomize/pkg/resid"
 	"sigs.k8s.io/kustomize/pkg/resmap"
 	"sigs.k8s.io/kustomize/pkg/resource"
-	"sigs.k8s.io/kustomize/pkg/types"
 )
 
-func TestImageTagTransformer(t *testing.T) {
+func TestImageTransformer(t *testing.T) {
 	var rf = resource.NewFactory(
 		kunstruct.NewKunstructuredFactoryImpl())
 
@@ -49,6 +49,10 @@ func TestImageTagTransformer(t *testing.T) {
 									"name":  "nginx2",
 									"image": "my-nginx:1.8.0",
 								},
+								map[string]interface{}{
+									"name":  "init-alpine",
+									"image": "alpine:1.8.0",
+								},
 							},
 							"containers": []interface{}{
 								map[string]interface{}{
@@ -58,6 +62,10 @@ func TestImageTagTransformer(t *testing.T) {
 								map[string]interface{}{
 									"name":  "replaced-with-digest",
 									"image": "foobar:1",
+								},
+								map[string]interface{}{
+									"name":  "postgresdb",
+									"image": "postgres:1.8.0",
 								},
 							},
 						},
@@ -98,6 +106,22 @@ func TestImageTagTransformer(t *testing.T) {
 						},
 					},
 				},
+				"spec3": map[string]interface{}{
+					"template": map[string]interface{}{
+						"spec": map[string]interface{}{
+							"initContainers": []interface{}{
+								map[string]interface{}{
+									"name":  "postgresdb",
+									"image": "postgres:alpine-9",
+								},
+								map[string]interface{}{
+									"name":  "init-docker",
+									"image": "docker:17-git",
+								},
+							},
+						},
+					},
+				},
 			}),
 	}
 	expected := resmap.ResMap{
@@ -117,6 +141,10 @@ func TestImageTagTransformer(t *testing.T) {
 									"name":  "nginx2",
 									"image": "my-nginx:previous",
 								},
+								map[string]interface{}{
+									"name":  "init-alpine",
+									"image": "myprivaterepohostname:1234/my/cool-alpine:1.8.0",
+								},
 							},
 							"containers": []interface{}{
 								map[string]interface{}{
@@ -126,6 +154,10 @@ func TestImageTagTransformer(t *testing.T) {
 								map[string]interface{}{
 									"name":  "replaced-with-digest",
 									"image": "foobar@sha256:24a0c4b4a4c0eb97a1aabb8e29f18e917d05abfe1b7a7c07857230879ce7d3d3",
+								},
+								map[string]interface{}{
+									"name":  "postgresdb",
+									"image": "my-postgres:v3",
 								},
 							},
 						},
@@ -166,14 +198,33 @@ func TestImageTagTransformer(t *testing.T) {
 						},
 					},
 				},
+				"spec3": map[string]interface{}{
+					"template": map[string]interface{}{
+						"spec": map[string]interface{}{
+							"initContainers": []interface{}{
+								map[string]interface{}{
+									"name":  "postgresdb",
+									"image": "my-postgres:v3",
+								},
+								map[string]interface{}{
+									"name":  "init-docker",
+									"image": "my-docker@sha256:25a0d4b4a4c0eb97a1aabb8e29f18e917d05abfe1b7a7c07857230879ce7d3d3",
+								},
+							},
+						},
+					},
+				},
 			}),
 	}
 
-	it, err := NewImageTagTransformer([]types.ImageTag{
+	it, err := NewImageTransformer([]image.Image{
 		{Name: "nginx", NewTag: "v2"},
 		{Name: "my-nginx", NewTag: "previous"},
 		{Name: "myprivaterepohostname:1234/my/image", NewTag: "v1.0.1"},
 		{Name: "foobar", Digest: "sha256:24a0c4b4a4c0eb97a1aabb8e29f18e917d05abfe1b7a7c07857230879ce7d3d3"},
+		{Name: "alpine", NewName: "myprivaterepohostname:1234/my/cool-alpine"},
+		{Name: "postgres", NewName: "my-postgres", NewTag: "v3"},
+		{Name: "docker", NewName: "my-docker", Digest: "sha256:25a0d4b4a4c0eb97a1aabb8e29f18e917d05abfe1b7a7c07857230879ce7d3d3"},
 	})
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -184,6 +235,6 @@ func TestImageTagTransformer(t *testing.T) {
 	}
 	if !reflect.DeepEqual(m, expected) {
 		err = expected.ErrorIfNotEqual(m)
-		t.Fatalf("actual doesn't match expected: %v", err)
+		t.Fatalf("actual doesn't match expected: %v. Actual %+v", err, m)
 	}
 }
