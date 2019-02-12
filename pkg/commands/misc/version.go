458763aa0850cd59566b0616bc645c71b8b1addc
abkaskari <abdelhak99jaafari@gmail.com>
2019-02-12 12:35:09 -0600

    Adding goos and goarch from runtime

--- pkg/commands/misc/version.go
+++ pkg/commands/misc/version.go
@@ -19,14 +19,15 @@ package misc
 import (
 	"fmt"
 	"io"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	kustomizeVersion = "unknown"
-	goos             = "unknown"
-	goarch           = "unknown"
+	goos             = runtime.GOOS
+	goarch           = runtime.GOARCH
 	gitCommit        = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
 
 	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
