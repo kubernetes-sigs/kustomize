9fba205bcb480eda43c1fde55741520046788a93
abkaskari <abdelhak99jaafari@gmail.com>
2019-04-07 11:16:50 -0700

    Define a plugin compiler.

--- pkg/target/testenvcontroller_test.go
+++ pkg/target/testenvcontroller_test.go
@@ -16,22 +16,22 @@ package target_test
 import (
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"testing"
-	"time"
-
 	"sigs.k8s.io/kustomize/k8sdeps/kv/plugin"
+	"testing"
+
 	"sigs.k8s.io/kustomize/pkg/pgmconfig"
+	"sigs.k8s.io/kustomize/pkg/plugins"
 )
 
 // TestEnvController manages the KustTarget test environment.
 // It sets/resets XDG_CONFIG_HOME, makes/removes a temp objRoot.
 type TestEnvController struct {
-	t             *testing.T
-	xdgConfigHome string
-	oldXdg        string
-	wasSet        bool
+	t        *testing.T
+	compiler *plugins.Compiler
+	workDir  string
+	oldXdg   string
+	wasSet   bool
 }
 
 func NewTestEnvController(t *testing.T) *TestEnvController {
@@ -39,121 +39,62 @@ func NewTestEnvController(t *testing.T) *TestEnvController {
 }
 
 func (x *TestEnvController) Set() *TestEnvController {
-	x.makeTmpConfigHomeDir()
-	x.makeObjectRootDir()
+	x.createWorkDir()
+	x.compiler = x.makeCompiler()
 	x.setEnv()
 	return x
 }
 
 func (x *TestEnvController) Reset() {
 	x.resetEnv()
-	x.removeTmpConfigHomeDir()
+	x.removeWorkDir()
 }
 
-func (x *TestEnvController) fileExists(name string) bool {
-	if _, err := os.Stat(name); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
-}
-
-func (x *TestEnvController) recentFileExists(path string) bool {
-	fi, err := os.Stat(path)
+func (x *TestEnvController) BuildGoPlugin(g, v, k string) {
+	err := x.compiler.Compile(g, v, k)
 	if err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	age := time.Now().Sub(fi.ModTime())
-	return age.Minutes() < 1
-}
-
-func (x *TestEnvController) BuildGoPlugin(plugin ...string) {
-	obj := filepath.Join(
-		append([]string{x.ObjectRoot()}, plugin...)...) + ".so"
-	if x.recentFileExists(obj) {
-		// Skip rebuilding it.
-		return
-	}
-	src := filepath.Join(
-		append([]string{x.SrcRoot()}, plugin...)...) + ".go"
-	if !x.fileExists(src) {
-		x.t.Errorf("cannot find go plugin source %s", src)
-	}
-	commands := []string{
-		"build",
-		"-buildmode",
-		"plugin",
-		"-tags=plugin",
-		"-o", obj, src,
-	}
-	goBin := filepath.Join(os.Getenv("GOROOT"), "bin", "go")
-	if !x.fileExists(src) {
-		x.t.Errorf("cannot find go compiler %s", goBin)
-	}
-	cmd := exec.Command(goBin, commands...)
-	cmd.Env = os.Environ()
-	// cmd.Dir = filepath.Join(dir, "kustomize", "plugins")
-
-	if err := cmd.Run(); err != nil {
-		x.t.Errorf("compiler error building %s: %v", src, err)
+		x.t.Errorf("compile failed: %v", err)
 	}
 }
 
-// ObjectRoot is the objRoot dir for plugin object files.
-func (x *TestEnvController) ObjectRoot() string {
-	return filepath.Join(
-		x.xdgConfigHome, pgmconfig.PgmName, plugin.PluginRoot)
-}
-
-// SrcRoot is a objRoot directory for plugin source code
-// used by tests.
-//
-// Plugin object code files have to be in a particular
-// location to be found and loaded for security reasons,
-// but placement of plugin source code is up to the user.
-//
-// This function returns a location for storing example
-// plugins for tests.  And maybe builtins at some point.
-func (x *TestEnvController) SrcRoot() string {
-	dir := filepath.Join(
-		os.Getenv("GOPATH"), "src",
-		pgmconfig.Repo, pgmconfig.PgmName, plugin.PluginRoot)
-	if _, err := os.Stat(dir); err != nil {
-		x.t.Errorf("plugin source objRoot '%s' not found", dir)
+func (x *TestEnvController) makeCompiler() *plugins.Compiler {
+	// The plugin loader wants to find object code under
+	//    $XDG_CONFIG_HOME/kustomize/plugins
+	// and the compiler writes object code to
+	//    $objRoot
+	// so set things up accordingly.
+	objRoot := filepath.Join(
+		x.workDir, pgmconfig.ProgramName, plugin.PluginRoot)
+	err := os.MkdirAll(objRoot, os.ModePerm)
+	if err != nil {
+		x.t.Error(err)
 	}
-	return dir
+	srcRoot, err := plugins.DefaultSrcRoot()
+	if err != nil {
+		x.t.Error(err)
+	}
+	return plugins.NewCompiler(srcRoot, objRoot)
 }
 
-func (x *TestEnvController) makeTmpConfigHomeDir() {
+func (x *TestEnvController) createWorkDir() {
 	var err error
-	x.xdgConfigHome, err = ioutil.TempDir("", "kustomizetests")
+	x.workDir, err = ioutil.TempDir("", "kustomize-plugin-tests")
 	if err != nil {
-		x.t.Errorf("failed to make temp objRoot: %v", err)
+		x.t.Errorf("failed to make work dir: %v", err)
 	}
 }
 
-func (x *TestEnvController) makeObjectRootDir() {
-	err := os.MkdirAll(x.ObjectRoot(), os.ModePerm)
+func (x *TestEnvController) removeWorkDir() {
+	err := os.RemoveAll(x.workDir)
 	if err != nil {
 		x.t.Errorf(
-			"making temp object objRoot %s: %v", x.ObjectRoot(), err)
-	}
-}
-
-func (x *TestEnvController) removeTmpConfigHomeDir() {
-	err := os.RemoveAll(x.xdgConfigHome)
-	if err != nil {
-		x.t.Errorf(
-			"removing temp object objRoot: %s %v", x.xdgConfigHome, err)
+			"removing work dir: %s %v", x.workDir, err)
 	}
 }
 
 func (x *TestEnvController) setEnv() {
 	x.oldXdg, x.wasSet = os.LookupEnv(pgmconfig.XDG_CONFIG_HOME)
-	os.Setenv(pgmconfig.XDG_CONFIG_HOME, x.xdgConfigHome)
+	os.Setenv(pgmconfig.XDG_CONFIG_HOME, x.workDir)
 }
 
 func (x *TestEnvController) resetEnv() {
