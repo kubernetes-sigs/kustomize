61d46c26b81b0bf83583aa10a315b46140d65104
Jingfang Liu <jingfangliu@google.com>
2019-05-10 10:20:20 -0700

    fix the boilerplate copyright header (#1064)

--- pkg/target/chartinflatorexecplugin_test.go
+++ pkg/target/chartinflatorexecplugin_test.go
@@ -1,19 +1,22 @@
 // +build notravis
 
-// Disabled on travis, because don't want to install helm on travis.
-
 /*
 Copyright 2019 The Kubernetes Authors.
- Licensed under the Apache License, Version 2.0 (the "License");
+
+Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Disabled on travis, because don't want to install helm on travis.
 
 package target_test
 
