4bc31f4b2a83a7a58803a476053f653e748ff475
Jeffrey Regan <jregan@google.com>
2019-06-18 10:14:36 -0700

    Move pluginator to cmd directory.

--- cmd/pluginator/main.go
+++ cmd/pluginator/main.go
@@ -1,7 +1,7 @@
 // Copyright 2019 The Kubernetes Authors.
 // SPDX-License-Identifier: Apache-2.0
 
-// See plugin/doc.go for an explanation.
+// See /plugin/doc.go for an explanation.
 package main
 
 import (
