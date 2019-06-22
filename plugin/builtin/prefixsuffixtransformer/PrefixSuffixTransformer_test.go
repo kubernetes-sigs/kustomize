1e949a1b318849d7b6fd339d4b0ef261d06524ec
abkaskari <abdelhak99jaafari@gmail.com>
2019-06-22 08:09:39 +0100

    Add test for regression

--- plugin/builtin/prefixsuffixtransformer/PrefixSuffixTransformer_test.go
+++ plugin/builtin/prefixsuffixtransformer/PrefixSuffixTransformer_test.go
@@ -66,4 +66,61 @@ kind: ConfigMap
 metadata:
   name: baked-cm-pie
 `)
+
+  rm = th.LoadAndRunTransformer(`
+apiVersion: builtin
+kind: PrefixSuffixTransformer
+metadata:
+  name: notImportantHere
+prefix: test-
+fieldSpecs:
+  - kind: Deployment
+    path: metadata/name
+  - kind: Deployment
+    path: spec/template/spec/containers/name
+`, `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment
+spec:
+  template:
+    spec:
+      containers:
+      - image: myapp
+        name: main
+        ---
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  name: crd
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm
+`)
+
+	th.AssertActualEqualsExpected(rm, `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  template:
+    spec:
+      containers:
+      - image: myapp
+        name: test-main
+---
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  name: crd
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: test-cm
+`)
 }
