9efb7681fcbc0a1992508106e09da9870a2cecba
abkaskari <abdelhak99jaafari@gmail.com>
2019-07-09 13:04:10 -0700

    Initial (temporary) implementation of search doc.
    Document describing how to convert a kustomization file into a
    searchable document on appengine (will be changed to elasticsearch)
    soon.

---
+++ internal/search/go.mod
@@ -0,0 +1,9 @@
+module sigs.k8s.io/kustomize/internal/search
+
+go 1.12
+
+require (
+	google.golang.org/appengine v1.6.1
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+	sigs.k8s.io/yaml v1.1.0
+)
--- travis/pre-commit.sh
+++ travis/pre-commit.sh
@@ -30,6 +30,7 @@ function testGoLangCILint {
 
 function testGoTest {
   go test -v ./...
+  (cd ./internal/search; go test -v ./...)
 }
 
 # These tests require the helm program, and at the moment
