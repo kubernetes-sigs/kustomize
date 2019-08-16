90aa5990a5b41144eadd53aa0c98277621560041
Jerome Brette <jbrette@gmail.com>
2019-08-16 18:56:11 +0000

    Test transformer with custom kind order

--- plugin/builtin/kubectlapplyordertransformer/KubectlApplyOrderTransformer_test.go
+++ plugin/builtin/kubectlapplyordertransformer/KubectlApplyOrderTransformer_test.go
@@ -23,7 +23,6 @@ apiVersion: builtin
 kind: KubectlApplyOrderTransformer
 metadata:
   name: notImportantHere
-kindorder: []
 `, `
 apiVersion: v1
 kind: Service
@@ -129,6 +128,146 @@ metadata:
 ---
 apiVersion: v1
 kind: CRD2
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: pomegranate
+`)
+}
+
+func TestKubectlApplyOrderTransformerCustomOrder(t *testing.T) {
+	tc := plugins_test.NewEnvForTest(t).Set()
+	defer tc.Reset()
+
+	tc.BuildGoPlugin(
+		"builtin", "", "KubectlApplyOrderTransformer")
+
+	th := kusttest_test.NewKustTestPluginHarness(t, "/app")
+	rm := th.LoadAndRunTransformer(`
+apiVersion: builtin
+kind: KubectlApplyOrderTransformer
+metadata:
+  name: notImportantHere
+kindorder:
+- Namespace
+- LimitRange
+- CRD1
+- CRD2
+- Secret
+- ConfigMap
+- Role
+- Service
+- Deployment
+- Ingress
+- ValidatingWebhookConfiguration
+`, `
+apiVersion: v1
+kind: Service
+metadata:
+  name: papaya
+---
+apiVersion: v1
+kind: Role
+metadata:
+  name: banana
+---
+apiVersion: v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: pomegranate
+---
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: peach
+---
+apiVersion: v1
+kind: Deployment
+metadata:
+  name: pear
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: apple
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: quince
+---
+apiVersion: v1
+kind: CRD1
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: CRD2
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: Ingress
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: apricot
+`)
+
+	th.AssertActualEqualsExpected(rm, `
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: apple
+---
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: peach
+---
+apiVersion: v1
+kind: CRD1
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: CRD2
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: quince
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: apricot
+---
+apiVersion: v1
+kind: Role
+metadata:
+  name: banana
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: papaya
+---
+apiVersion: v1
+kind: Deployment
+metadata:
+  name: pear
+---
+apiVersion: v1
+kind: Ingress
 metadata:
   name: durian
 ---
--- plugin/builtin/kubectldeleteordertransformer/KubectlDeleteOrderTransformer_test.go
+++ plugin/builtin/kubectldeleteordertransformer/KubectlDeleteOrderTransformer_test.go
@@ -23,7 +23,6 @@ apiVersion: builtin
 kind: KubectlDeleteOrderTransformer
 metadata:
   name: notImportantHere
-kindorder: []
 `, `
 apiVersion: v1
 kind: Service
@@ -126,6 +125,146 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: quince
+---
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: peach
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: apple
+`)
+}
+
+func TestKubectlDeleteOrderTransformerCustomOrder(t *testing.T) {
+	tc := plugins_test.NewEnvForTest(t).Set()
+	defer tc.Reset()
+
+	tc.BuildGoPlugin(
+		"builtin", "", "KubectlApplyOrderTransformer")
+
+	th := kusttest_test.NewKustTestPluginHarness(t, "/app")
+	rm := th.LoadAndRunTransformer(`
+apiVersion: builtin
+kind: KubectlApplyOrderTransformer
+metadata:
+  name: notImportantHere
+kindorder:
+- ValidatingWebhookConfiguration
+- Ingress
+- Deployment
+- Service
+- Role
+- ConfigMap
+- Secret
+- CRD2
+- CRD1
+- LimitRange
+- Namespace
+`, `
+apiVersion: v1
+kind: Service
+metadata:
+  name: papaya
+---
+apiVersion: v1
+kind: Role
+metadata:
+  name: banana
+---
+apiVersion: v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: pomegranate
+---
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: peach
+---
+apiVersion: v1
+kind: Deployment
+metadata:
+  name: pear
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: apple
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: quince
+---
+apiVersion: v1
+kind: CRD1
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: CRD2
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: Ingress
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: apricot
+`)
+
+	th.AssertActualEqualsExpected(rm, `
+apiVersion: v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: pomegranate
+---
+apiVersion: v1
+kind: Ingress
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: Deployment
+metadata:
+  name: pear
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: papaya
+---
+apiVersion: v1
+kind: Role
+metadata:
+  name: banana
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: apricot
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: quince
+---
+apiVersion: v1
+kind: CRD2
+metadata:
+  name: durian
+---
+apiVersion: v1
+kind: CRD1
+metadata:
+  name: durian
 ---
 apiVersion: v1
 kind: LimitRange
