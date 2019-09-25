aa342deff7a3332d22f1f08e30dce6142b68fc1d
Ian Howell <ian.howell0@gmail.com>
2019-09-25 12:02:52 -0500

    Pin tool versions using go modules
    This pins down the versions of various tools used in the building,
    testing, and linting of kustomize. This will provide more consistency
    across contributors' and travis' environments.

---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+// +build tools
+
+package tools
+
+import (
+	// These imports are all tools used in the building and testing process
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/monopole/mdrip"
+)
