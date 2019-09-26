7c4385755842ba263b88c5cdcd87d404ca8df5bd
Jeremy Gustie <jeremy@carbonrelay.com>
2019-09-26 15:48:11 -0400

    Do not create commonLabel selectors on legacy resources
    For types that give special meaning to an empty selector, having create=true overwrites the implicit default labels

--- pkg/transformers/config/defaultconfig/commonlabels.go
+++ pkg/transformers/config/defaultconfig/commonlabels.go
@@ -65,6 +65,12 @@ commonLabels:
   kind: Deployment
 
 - path: spec/selector/matchLabels
+  create: false
+  group: extensions
+  version: v1beta1
+  kind: ReplicaSet
+
+- path: spec/selector/matchLabels
   create: true
   kind: ReplicaSet
 
@@ -73,12 +79,24 @@ commonLabels:
   kind: ReplicaSet
 
 - path: spec/selector/matchLabels
+  create: false
+  group: extensions
+  version: v1beta1
+  kind: DaemonSet
+
+- path: spec/selector/matchLabels
   create: true
   kind: DaemonSet
 
 - path: spec/template/metadata/labels
   create: true
   kind: DaemonSet
+
+- path: spec/selector/matchLabels
+  create: false
+  group: apps
+  version: v1beta1
+  kind: StatefulSet
 
 - path: spec/selector/matchLabels
   create: true
