78d14d0d75e7df275bebe418c9e9d4e6664b0f6a
jregan <jregan@google.com>
2019-10-07 21:21:12 -0700

    Introduce dummy program to help with API releases.

---
+++ kustapiversion/main.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+// A dummy main to help with releasing the kustomize API module.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"sigs.k8s.io/kustomize/v3/provenance"
+)
+
+// TODO: delete this when we find a better way to generate release notes.
+func main() {
+	fmt.Println(`
+This 'main' exists only to make goreleaser create release notes for the API.
+See https://github.com/goreleaser/goreleaser/issues/981
+and https://github.com/kubernetes-sigs/kustomize/tree/master/releasing`)
+	provenance.GetProvenance().Print(os.Stdout, false)
+}
--- kustomize/go.mod
+++ kustomize/go.mod
@@ -6,12 +6,8 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5
-	gopkg.in/inf.v0 v0.9.1 // indirect
-	sigs.k8s.io/kustomize/v3 v3.2.0
+	sigs.k8s.io/kustomize/v3 v3.3.0
 	sigs.k8s.io/yaml v1.1.0
 )
 
-// When tagging and releasing this module (the kustomize CLI),
-// be sure to require a specific version of sig.k8s.io/kustomize
-// above, and drop the following replace directive.
-replace sigs.k8s.io/kustomize/v3 v3.2.0 => ../
+replace sigs.k8s.io/kustomize/v3 v3.3.0 => ../
