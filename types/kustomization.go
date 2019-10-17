95dd8b355bb355d665df8ef05790bbbfcf8c2542
abkaskari <abdelhak99jaafari@gmail.com>
2019-10-16 17:21:07 -0700

    Document the types package.

--- types/kustomization.go
+++ types/kustomization.go
@@ -1,7 +1,6 @@
 // Copyright 2019 The Kubernetes Authors.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package types holds struct definitions that should find a better home.
 package types
 
 const (
