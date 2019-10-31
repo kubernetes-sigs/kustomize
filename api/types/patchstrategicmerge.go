9ca5284e4f9dde2bb978483251b94d82ee2a35ae
Liz <33235082+liztio@users.noreply.github.com>
2019-10-31 14:31:19 -0400

    strategic-merge-patch documentation has moved

--- api/types/patchstrategicmerge.go
+++ api/types/patchstrategicmerge.go
@@ -5,5 +5,5 @@ package types
 
 // PatchStrategicMerge represents a relative path to a
 // stategic merge patch with the format
-// https://github.com/kubernetes/community/blob/master/contributors/devel/strategic-merge-patch.md
+// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-api-machinery/strategic-merge-patch.md
 type PatchStrategicMerge string
