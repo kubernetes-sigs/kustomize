690e01c2badf11b88994a53ef00f3d9e88afc4cd
Jeff Regan <jregan@google.com>
2019-11-02 14:12:15 -0700

    Update whatApi.sh

--- hack/whatApi.sh
+++ hack/whatApi.sh
@@ -6,14 +6,21 @@
 
 function whatApi {
   echo "==== begin $1 =================================="
-  find $1 -name "*.go" |\
+    find $1 -name "*.go" |\
     xargs grep \"sigs.k8s.io/kustomize/api/ |\
     sed 's|:\s"|: dummy "|'   |\
     sed 's|:\s\w\+\s"|  |'    |\
     sed 's|"$||'              |\
     awk '{ printf "%60s  %s\n", $2, $1 }' |\
     sed 's|sigs.k8s.io/kustomize/api/||' |\
-    sort | uniq
+    sort |\
+    uniq |\
+    grep -v " filesys " |\
+    grep -v " resource " |\
+    grep -v " resid " |\
+    grep -v " resmap " |\
+    grep -v " testutils/" |\
+    grep -v "types "
   echo "==== end $1 =================================="
 }
 
