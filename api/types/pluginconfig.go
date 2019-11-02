079c3307c15936facb1a5b0e9c67242610ab7a98
jregan <jregan@google.com>
2019-11-02 13:46:56 -0700

    Improved plugin loading docs.

--- api/types/pluginconfig.go
+++ api/types/pluginconfig.go
@@ -5,11 +5,25 @@ package types
 
 // PluginConfig holds plugin configuration.
 type PluginConfig struct {
-	// DirectoryPath is an absolute path to a
-	// directory containing kustomize plugins.
-	// This directory may contain subdirectories
-	// further categorizing plugins.
-	DirectoryPath string
+	// AbsPluginHome is the home of kustomize plugins.
+	// Kustomize plugin configuration files are k8s-style objects
+	// containing the fields 'apiVersion' and 'kind', e.g.
+	//   apiVersion: apps/v1
+	//   kind: Deployment
+	// When kustomize reads a plugin configuration file (as as result
+	// of seeing the file name in the 'generators:' or 'transformers:'
+	// field in a kustomization file), it must then locate the plugin
+	// code (Go plugin or exec plugin).
+	// Every kustomize plugin (its code, its tests, supporting data
+	// files, etc.) must be housed in its own directory at
+	//   ${AbsPluginHome}/${pluginApiVersion}/LOWERCASE(${pluginKind})
+	// where
+	//   - ${AbsPluginHome} is an absolute path, defined below.
+	//   - ${pluginApiVersion} is taken from the plugin config file.
+	//   - ${pluginKind} is taken from the plugin config file.
+	// The value of AbsPluginHome can be any absolute path, but might
+	// default to $XDG_CONFIG_HOME/kustomize/plugin.
+	AbsPluginHome string
 
 	// PluginRestrictions defines the plugin restriction state.
 	// See type for more information.
