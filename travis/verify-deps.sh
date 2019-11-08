ed5402cdce68551e1483f959347b99fbbb76f819
Phillip Wittrock <pwittroc@google.com>
2019-11-07 17:47:38 -0800

    update travis for dep checking

--- travis/verify-deps.sh
+++ travis/verify-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if find forked -name go.sum | xargs grep -E "k8s\.io/(api|client-go)" ; then
+if find api forked -name go.sum | xargs grep -E "k8s\.io/(api|client-go)" ; then
   echo "deps not allowed"
-  find forked -name go.sum | xargs grep -E "k8s\.io/(api|client-go)"
+  find api forked -name go.sum | xargs grep -E "k8s\.io/(api|client-go)"
   exit 1
 fi
