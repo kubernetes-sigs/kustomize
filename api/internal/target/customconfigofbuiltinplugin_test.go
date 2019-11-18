bf119bf5b7ab3b51cba5845832ae1cc9807d553e
Jeffrey Regan <jregan@google.com>
2019-11-18 08:52:14 -0800

    Add test elaborating on custom config.

--- api/internal/target/customconfigofbuiltinplugin_test.go
+++ api/internal/target/customconfigofbuiltinplugin_test.go
@@ -10,8 +10,8 @@ import (
 )
 
 // Demo custom configuration of a builtin transformation.
-// This is a NamePrefixer that only touches Deployments
-// and Services.
+// This is a NamePrefixer that touches Deployments
+// and Services exclusively.
 func TestCustomNamePrefixer(t *testing.T) {
 	tc := kusttest_test.NewPluginTestEnv(t).Set()
 	defer tc.Reset()
@@ -83,121 +83,6 @@ spec:
     metadata:
       labels:
         backend: awesome
-    spec:
-      containers:
-      - image: whatever
-        name: whatever
----
-apiVersion: v1
-kind: Role
-metadata:
-  name: myRole
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: zzz-myService
-`)
-}
-
-// Demo custom configuration as a base.
-func TestReusableCustomNamePrefixer(t *testing.T) {
-	tc := kusttest_test.NewPluginTestEnv(t).Set()
-	defer tc.Reset()
-
-	tc.BuildGoPlugin(
-		"builtin", "", "PrefixSuffixTransformer")
-	tc.BuildGoPlugin(
-		"builtin", "", "LabelTransformer")
-
-	th := kusttest_test.NewKustTestHarnessAllowPlugins(t, "/app/foo")
-
-	// This kustomization file contains resources that
-	// all happen to be plugin configurations. This makes
-	// these plugins all available as part of a base,
-	// re-usable in any number of other kustomizations.
-	// Just specify the path (or URL) to this base in the
-	// "transformers:" field (not the "resources" field).
-	th.WriteK("/app/mytransformers", `
-resources:
-- prefixer.yaml
-- labeller.yaml
-`)
-	th.WriteF("/app/mytransformers/prefixer.yaml", `
-apiVersion: builtin
-kind: PrefixSuffixTransformer
-metadata:
-  name: myPrefixer
-prefix: zzz-
-fieldSpecs:
-- kind: Deployment
-  path: metadata/name
-- kind: Service
-  path: metadata/name
-`)
-	th.WriteF("/app/mytransformers/labeller.yaml", `
-apiVersion: builtin
-kind: LabelTransformer
-metadata:
-  name: myLabeller
-labels:
-  company: acmeCorp
-fieldSpecs:
-- path: spec/template/metadata/labels
-  kind: Deployment
-`)
-
-	th.WriteK("/app/foo", `
-resources:
-- deployment.yaml
-- role.yaml
-- service.yaml
-transformers:
-- ../mytransformers
-`)
-	th.WriteF("/app/foo/deployment.yaml", `
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: myDeployment
-spec:
-  template:
-    metadata:
-      labels:
-        backend: awesome
-    spec:
-      containers:
-      - name: whatever
-        image: whatever
-`)
-	th.WriteF("/app/foo/role.yaml", `
-apiVersion: v1
-kind: Role
-metadata:
-  name: myRole
-`)
-	th.WriteF("/app/foo/service.yaml", `
-apiVersion: v1
-kind: Service
-metadata:
-  name: myService
-`)
-
-	m, err := th.MakeKustTarget().MakeCustomizedResMap()
-	if err != nil {
-		t.Fatalf("Err: %v", err)
-	}
-	th.AssertActualEqualsExpected(m, `
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: zzz-myDeployment
-spec:
-  template:
-    metadata:
-      labels:
-        backend: awesome
-        company: acmeCorp
     spec:
       containers:
       - image: whatever
