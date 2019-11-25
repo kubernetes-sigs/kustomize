cb96683647f56ebf686f6bf0733a597976f63ede
abkaskari <abdelhak99jaafari@gmail.com>
2019-11-25 09:56:20 -0800

    cmd/config: expose target for embedding config commands

--- cmd/config/main.go
+++ cmd/config/main.go
@@ -8,53 +8,11 @@ package main
 import (
 	"os"
 
-	"github.com/spf13/cobra"
-	"sigs.k8s.io/kustomize/cmd/config/cmd"
-	"sigs.k8s.io/kustomize/cmd/config/cmddocs/api"
+	"sigs.k8s.io/kustomize/cmd/config/cmds"
 )
 
-var root = &cobra.Command{
-	Use:   "config",
-	Short: "Utilities for working with Resource Configuration.",
-	Long:  `Utilities for working with Resource Configuration.`,
-}
-
 func main() {
-	root.PersistentFlags().BoolVar(&cmd.StackOnError, "stack-trace", false,
-		"print a stack-trace on failure")
-
-	name := "config"
-	cmd.ExitOnError = true
-	root.AddCommand(cmd.GrepCommand(name))
-	root.AddCommand(cmd.TreeCommand(name))
-	root.AddCommand(cmd.CatCommand(name))
-	root.AddCommand(cmd.FmtCommand(name))
-	root.AddCommand(cmd.MergeCommand(name))
-	root.AddCommand(cmd.CountCommand(name))
-	root.AddCommand(cmd.RunFnCommand(name))
-
-	root.AddCommand(&cobra.Command{
-		Use:   "docs-merge",
-		Short: "Documentation for merging Resources (2-way merge).",
-		Long:  api.Merge2Long,
-	})
-	root.AddCommand(&cobra.Command{
-		Use:   "docs-merge3",
-		Short: "Documentation for merging Resources (3-way merge).",
-		Long:  api.Merge3Long,
-	})
-	root.AddCommand(&cobra.Command{
-		Use:   "docs-fn",
-		Short: "Documentation for writing containerized functions run by run-fns.",
-		Long:  api.ConfigFnLong,
-	})
-	root.AddCommand(&cobra.Command{
-		Use:   "docs-io-annotations",
-		Short: "Documentation for annotations used by io.",
-		Long:  api.ConfigIoLong,
-	})
-
-	if err := root.Execute(); err != nil {
+	if err := cmds.NewConfigCommand("").Execute(); err != nil {
 		os.Exit(1)
 	}
 }
