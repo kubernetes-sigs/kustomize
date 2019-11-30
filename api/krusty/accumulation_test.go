1f85ce454d6fd670382fb9acc92a15795f977434
jregan <jregan@google.com>
2019-11-30 11:28:42 -0800

    More tests/examples.

--- api/krusty/accumulation_test.go
+++ api/krusty/accumulation_test.go
@@ -4,7 +4,11 @@
 package krusty_test
 
 import (
+	"path/filepath"
+	"strings"
 	"testing"
+
+	"sigs.k8s.io/kustomize/api/konfig"
 
 	. "sigs.k8s.io/kustomize/api/internal/target"
 )
@@ -32,6 +36,23 @@ metadata:
 	}
 }
 
+func TestTargetMustHaveOnlyOneKustomizationFile(t *testing.T) {
+	th := makeTestHarness(t)
+	for _, n := range konfig.RecognizedKustomizationFileNames() {
+		th.WriteF(filepath.Join("/app", n), `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+`)
+	}
+	err := th.RunWithErr("/app", th.MakeDefaultOptions())
+	if err == nil {
+		t.Fatalf("expected an error")
+	}
+	if !strings.Contains(err.Error(), "Found multiple kustomization files under: /app") {
+		t.Fatalf("unexpected error: %q", err)
+	}
+}
+
 func TestBaseMustHaveKustomizationFile(t *testing.T) {
 	th := makeTestHarness(t)
 	th.WriteK("/app", `
@@ -54,6 +75,21 @@ spec:
 		t.Fatalf("expected an error")
 	}
 	if !IsMissingKustomizationFileError(err) {
+		t.Fatalf("unexpected error: %q", err)
+	}
+}
+
+func TestResourceNotFound(t *testing.T) {
+	th := makeTestHarness(t)
+	th.WriteK("/app", `
+resources:
+- deployment.yaml
+`)
+	err := th.RunWithErr("/app", th.MakeDefaultOptions())
+	if err == nil {
+		t.Fatalf("expected an error")
+	}
+	if !strings.Contains(err.Error(), "'/app/deployment.yaml' doesn't exist") {
 		t.Fatalf("unexpected error: %q", err)
 	}
 }
