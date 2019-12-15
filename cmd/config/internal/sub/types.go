13f1f2e61de7cb4662c6725fe0f91562723ecc43
abkaskari <abdelhak99jaafari@gmail.com>
2019-12-15 14:57:47 -0800

    `cmd/config set`: Support for setting fields imperatively from the cli

---
+++ cmd/config/internal/sub/types.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package sub
+
+import (
+	"sigs.k8s.io/kustomize/kyaml/fieldmeta"
+)
+
+// FieldSubstitution is a possible field substitution read from a field
+type FieldSubstitution struct {
+	// Name is the name of the substitution
+	Name string
+
+	// Description is a description of the fields current value
+	Description string
+
+	// Value is the current substituted value for the field.
+	CurrentValue string
+
+	// Type is the type of the substitution
+	Type fieldmeta.FieldValueType
+
+	// Marker is the marker used
+	Marker string
+
+	// OwnedBy, if set will annotate the field with an owner.
+	OwnedBy string
+}
