7a384bc0d85cc8ab10a912fbd2382d8d4852dd6d
Naoki Oketani <okepy.naoki@gmail.com>
2019-12-17 18:22:49 +0900

    Fixed a wrong path and run `make all`

--- cmd/config/internal/generateddocs/api/docs.go
+++ cmd/config/internal/generateddocs/api/docs.go
@@ -292,16 +292,17 @@ Resource Configuration may be read / written from / to sources such as directori
 stdin|out or network. Tools may be composed using pipes such that the tools writing
 Resource Configuration may be a different tool from the one that read the configuration.
 In order for tools to be composed in this way, while preserving origin information --
-such as the original file, index, etc.
+such as the original file, index, etc.:
 
-Tools **SHOULD** write the following annotations when reading from sources,
-and **SHOULD** respect the annotations when writing to sinks.
+Tools **SHOULD** insert the following annotations when reading from sources,
+and **SHOULD** delete the annotations when writing to sinks.
 
 ### ` + "`" + `config.kubernetes.io/path` + "`" + `
 
 Records the slash-delimited, OS-agnostic, relative file path to a Resource.
 
 This annotation **SHOULD** be set when reading Resources from files.
+It **SHOULD** be unset when writing Resources to files.
 When writing Resources to a directory, the Resource **SHOULD** be written to the corresponding
 path relative to that directory.
 
@@ -313,10 +314,11 @@ Example:
 
 ### ` + "`" + `config.kubernetes.io/index` + "`" + `
 
-Records the index of a Resource in file. In a multi-object files YAML file, Resources are separated
+Records the index of a Resource in file. In a multi-object YAML file, Resources are separated
 by three dashes (` + "`" + `---` + "`" + `), and the index represents the positon of the Resource starting from zero.
 
 This annotation **SHOULD** be set when reading Resources from files.
+It **SHOULD** be unset when writing Resources to files.
 When writing multiple Resources to the same file, the Resource **SHOULD** be written in the
 relative order matching the index.
 
