b62c2a617250837c82ed22e7bdeecd1b4d045589
abkaskari <abdelhak99jaafari@gmail.com>
2019-12-21 16:04:23 -0800

    completion should suggest directories if use contains DIR

--- cmd/config/complete/complete.go
+++ cmd/config/complete/complete.go
@@ -52,6 +52,10 @@ func Complete(cmd *cobra.Command) *complete.Command {
 		Flags: map[string]complete.Predictor{},
 		Sub:   map[string]*complete.Command{},
 	}
+	if strings.Contains(cmd.Use, "DIR") {
+		// if usage contains directory, then use a file predictor
+		cc.Args = predict.Dirs("*")
+	}
 
 	// add completion for each subcommand
 	for i := range cmd.Commands() {
