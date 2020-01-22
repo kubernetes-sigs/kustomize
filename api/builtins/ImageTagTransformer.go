2c78824f4b08ebac0ae0b42e4e2a2d7cb6eb740e
abkaskari <abdelhak99jaafari@gmail.com>
2020-01-22 15:39:12 +0900

    fix: update generated file

--- api/builtins/ImageTagTransformer.go
+++ api/builtins/ImageTagTransformer.go
@@ -147,7 +147,7 @@ func (p *ImageTagTransformerPlugin) findContainers(obj map[string]interface{}) e
 }
 
 func isImageMatched(s, t string) bool {
-	// Tag values are limited to [a-zA-Z0-9_.-].
+	// Tag values are limited to [a-zA-Z0-9_.-{}].
 	pattern, _ := regexp.Compile("^" + t + "(@sha256)?(:[a-zA-Z0-9_.-{}]*)?$")
 	return pattern.MatchString(s)
 }
