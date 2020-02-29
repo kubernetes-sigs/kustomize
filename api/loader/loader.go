e5911b12940771e6d69d6c460d5b355574f4eb56
abkaskari <abdelhak99jaafari@gmail.com>
2020-02-29 16:47:26 +0800

    Use a tailored go-getter to get rid of transitive depenencies

--- api/loader/loader.go
+++ api/loader/loader.go
@@ -12,9 +12,8 @@ import (
 
 	"sigs.k8s.io/kustomize/api/filesys"
 	"sigs.k8s.io/kustomize/api/ifc"
+	"sigs.k8s.io/kustomize/api/internal/getter"
 	"sigs.k8s.io/kustomize/api/internal/git"
-
-	getter "github.com/hashicorp/go-getter"
 )
 
 // NewLoader returns a Loader pointed at the given target.
