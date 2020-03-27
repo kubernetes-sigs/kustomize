2be48ca96a98fb946acf806a7cd6a0c19d492664
Jeffrey Regan <jregan@google.com>
2020-03-26 17:19:30 -0700

    Function to set labels.

--- api/filters/filtersutil/filtersutil_test.go
+++ api/filters/filtersutil/filtersutil_test.go
@@ -11,6 +11,32 @@ import (
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
+func TestSortedKeys(t *testing.T) {
+	testCases := map[string]struct {
+		input    map[string]string
+		expected []string
+	}{
+		"empty": {
+			input:    map[string]string{},
+			expected: []string{}},
+		"one": {
+			input:    map[string]string{"a": "aaa"},
+			expected: []string{"a"}},
+		"three": {
+			input:    map[string]string{"c": "ccc", "b": "bbb", "a": "aaa"},
+			expected: []string{"a", "b", "c"}},
+	}
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			if !assert.Equal(t,
+				filtersutil.SortedMapKeys(tc.input),
+				tc.expected) {
+				t.FailNow()
+			}
+		})
+	}
+}
+
 func TestApplyToJSON(t *testing.T) {
 	instance1 := bytes.NewBufferString(`{"kind": "Foo"}`)
 	instance2 := bytes.NewBufferString(`{"kind": "Bar"}`)
