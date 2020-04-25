7feb873ef685b2246a14ac53592fffe3e907b59d
jregan <jregan@google.com>
2020-04-25 12:59:38 -0700

    Remove MapTransformer.

--- plugin/builtin/annotationstransformer/AnnotationsTransformer.go
+++ plugin/builtin/annotationstransformer/AnnotationsTransformer.go
@@ -7,7 +7,6 @@ package main
 import (
 	"sigs.k8s.io/kustomize/api/filters/annotations"
 	"sigs.k8s.io/kustomize/api/resmap"
-	"sigs.k8s.io/kustomize/api/transform"
 	"sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/kustomize/kyaml/filtersutil"
 	"sigs.k8s.io/yaml"
@@ -17,10 +16,6 @@ import (
 type plugin struct {
 	Annotations map[string]string `json:"annotations,omitempty" yaml:"annotations,omitempty"`
 	FieldSpecs  []types.FieldSpec `json:"fieldSpecs,omitempty" yaml:"fieldSpecs,omitempty"`
-
-	// YAMLSupport can be set to true to use the kyaml filter instead of the
-	// kunstruct transformer
-	YAMLSupport bool `json:"yamlSupport,omitempty" yaml:"yamlSupport,omitempty"`
 }
 
 //noinspection GoUnusedGlobalVariable
@@ -34,25 +29,14 @@ func (p *plugin) Config(
 }
 
 func (p *plugin) Transform(m resmap.ResMap) error {
-	if p.YAMLSupport {
-		for _, r := range m.Resources() {
-			err := filtersutil.ApplyToJSON(annotations.Filter{
-				Annotations: p.Annotations,
-				FsSlice:     p.FieldSpecs,
-			}, r.Kunstructured)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	} else {
-		t, err := transform.NewMapTransformer(
-			p.FieldSpecs,
-			p.Annotations,
-		)
+	for _, r := range m.Resources() {
+		err := filtersutil.ApplyToJSON(annotations.Filter{
+			Annotations: p.Annotations,
+			FsSlice:     p.FieldSpecs,
+		}, r.Kunstructured)
 		if err != nil {
 			return err
 		}
-		return t.Transform(m)
 	}
+	return nil
 }
