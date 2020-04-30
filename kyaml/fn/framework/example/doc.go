b579bf2b036fb9384bca91436d894edbd5a91f8a
Phillip Wittrock <pwittroc@google.com>
2020-04-30 13:33:56 -0700

    Better support for writing functions in go

---
+++ kyaml/fn/framework/example/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package main contains an example using the the framework.
+//
+// To generate the Dockerfile for the function image run:
+//
+//   $ go run ./main.go .
+package main
