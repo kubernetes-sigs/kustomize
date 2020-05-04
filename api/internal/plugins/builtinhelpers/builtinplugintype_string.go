81a3112a90424181ff2bce9dc786e9e49be9a778
abkaskari <abdelhak99jaafari@gmail.com>
2020-05-03 19:33:30 -0700

    ValueAddTransformer

--- api/internal/plugins/builtinhelpers/builtinplugintype_string.go
+++ api/internal/plugins/builtinhelpers/builtinplugintype_string.go
@@ -22,11 +22,12 @@ func _() {
 	_ = x[PrefixSuffixTransformer-11]
 	_ = x[ReplicaCountTransformer-12]
 	_ = x[SecretGenerator-13]
+	_ = x[ValueAddTransformer-14]
 }
 
-const _BuiltinPluginType_name = "UnknownAnnotationsTransformerConfigMapGeneratorHashTransformerImageTagTransformerLabelTransformerLegacyOrderTransformerNamespaceTransformerPatchJson6902TransformerPatchStrategicMergeTransformerPatchTransformerPrefixSuffixTransformerReplicaCountTransformerSecretGenerator"
+const _BuiltinPluginType_name = "UnknownAnnotationsTransformerConfigMapGeneratorHashTransformerImageTagTransformerLabelTransformerLegacyOrderTransformerNamespaceTransformerPatchJson6902TransformerPatchStrategicMergeTransformerPatchTransformerPrefixSuffixTransformerReplicaCountTransformerSecretGeneratorValueAddTransformer"
 
-var _BuiltinPluginType_index = [...]uint16{0, 7, 29, 47, 62, 81, 97, 119, 139, 163, 193, 209, 232, 255, 270}
+var _BuiltinPluginType_index = [...]uint16{0, 7, 29, 47, 62, 81, 97, 119, 139, 163, 193, 209, 232, 255, 270, 289}
 
 func (i BuiltinPluginType) String() string {
 	if i < 0 || i >= BuiltinPluginType(len(_BuiltinPluginType_index)-1) {
---
+++ plugin/builtin/valueaddtransformer/go.mod
@@ -0,0 +1,11 @@
+module sigs.k8s.io/kustomize/plugin/someteam.example.com/v1/valueaddtransformer
+
+go 1.13
+
+require (
+	sigs.k8s.io/kustomize/api v0.0.0
+	sigs.k8s.io/kustomize/kyaml v0.1.10
+	sigs.k8s.io/yaml v1.2.0
+)
+
+replace sigs.k8s.io/kustomize/api v0.0.0 => ../../../api
