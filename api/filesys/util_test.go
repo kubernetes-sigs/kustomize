51e3e0ff2928759fec8d2e2f8a3af6373a0d81e8
jregan <jregan@google.com>
2020-05-09 17:21:31 -0700

    add filesys.InsertPathPart function

--- api/filesys/util_test.go
+++ api/filesys/util_test.go
@@ -1,6 +1,7 @@
 package filesys_test
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -94,6 +95,11 @@ func TestFilePathSplit(t *testing.T) {
 			file: "rabbit.jpg",
 		},
 		{
+			full: "/",
+			dir:  "/",
+			file: "",
+		},
+		{
 			full: "/beans",
 			dir:  "/",
 			file: "beans",
@@ -121,6 +127,169 @@ func TestFilePathSplit(t *testing.T) {
 				"in '%s',\ngot dir='%s' (expected '%s'),\n got file='%s' (expected %s).",
 				p.full, dir, p.dir, file, p.file)
 		}
+	}
+}
+
+func TestPathSplitAndJoin(t *testing.T) {
+	cases := map[string]struct {
+		original string
+		expected []string
+	}{
+		"Empty": {
+			original: "",
+			expected: []string{},
+		},
+		"One": {
+			original: "hello",
+			expected: []string{"hello"},
+		},
+		"Two": {
+			original: "hello/there",
+			expected: []string{"hello", "there"},
+		},
+		"Three": {
+			original: "hello/my/friend",
+			expected: []string{"hello", "my", "friend"},
+		},
+	}
+	for n, c := range cases {
+		f := func(t *testing.T, original string, expected []string) {
+			actual := PathSplit(original)
+			if len(actual) != len(expected) {
+				t.Fatalf(
+					"expected len %d, got len %d",
+					len(expected), len(actual))
+			}
+			for i := range expected {
+				if expected[i] != actual[i] {
+					t.Fatalf(
+						"at i=%d, expected '%s', got '%s'",
+						i, expected[i], actual[i])
+				}
+			}
+			joined := PathJoin(actual)
+			if joined != original {
+				t.Fatalf(
+					"when rejoining, expected '%s', got '%s'",
+					original, joined)
+			}
+		}
+		t.Run("relative"+n, func(t *testing.T) {
+			f(t, c.original, c.expected)
+		})
+		t.Run("absolute"+n, func(t *testing.T) {
+			f(t,
+				string(os.PathSeparator)+c.original,
+				append([]string{""}, c.expected...))
+		})
+	}
+}
+
+func TestInsertPathPart(t *testing.T) {
+	cases := map[string]struct {
+		original string
+		pos      int
+		part     string
+		expected string
+	}{
+		"rootOne": {
+			original: "/",
+			pos:      0,
+			part:     "___",
+			expected: "/___",
+		},
+		"rootTwo": {
+			original: "/",
+			pos:      444,
+			part:     "___",
+			expected: "/___",
+		},
+		"rootedFirst": {
+			original: "/apple",
+			pos:      0,
+			part:     "___",
+			expected: "/___/apple",
+		},
+		"rootedSecond": {
+			original: "/apple",
+			pos:      444,
+			part:     "___",
+			expected: "/apple/___",
+		},
+		"rootedThird": {
+			original: "/apple/banana",
+			pos:      444,
+			part:     "___",
+			expected: "/apple/banana/___",
+		},
+		"emptyLow": {
+			original: "",
+			pos:      -3,
+			part:     "___",
+			expected: "___",
+		},
+		"emptyHigh": {
+			original: "",
+			pos:      444,
+			part:     "___",
+			expected: "___",
+		},
+		"peachPie": {
+			original: "a/nice/warm/pie",
+			pos:      3,
+			part:     "PEACH",
+			expected: "a/nice/warm/PEACH/pie",
+		},
+		"rootedPeachPie": {
+			original: "/a/nice/warm/pie",
+			pos:      3,
+			part:     "PEACH",
+			expected: "/a/nice/warm/PEACH/pie",
+		},
+		"longStart": {
+			original: "a/b/c/d/e/f",
+			pos:      0,
+			part:     "___",
+			expected: "___/a/b/c/d/e/f",
+		},
+		"rootedLongStart": {
+			original: "/a/b/c/d/e/f",
+			pos:      0,
+			part:     "___",
+			expected: "/___/a/b/c/d/e/f",
+		},
+		"longMiddle": {
+			original: "a/b/c/d/e/f",
+			pos:      3,
+			part:     "___",
+			expected: "a/b/c/___/d/e/f",
+		},
+		"rootedLongMiddle": {
+			original: "/a/b/c/d/e/f",
+			pos:      3,
+			part:     "___",
+			expected: "/a/b/c/___/d/e/f",
+		},
+		"longEnd": {
+			original: "a/b/c/d/e/f",
+			pos:      444,
+			part:     "___",
+			expected: "a/b/c/d/e/f/___",
+		},
+		"rootedLongEnd": {
+			original: "/a/b/c/d/e/f",
+			pos:      444,
+			part:     "___",
+			expected: "/a/b/c/d/e/f/___",
+		},
+	}
+	for n, c := range cases {
+		t.Run(n, func(t *testing.T) {
+			actual := InsertPathPart(c.original, c.pos, c.part)
+			if actual != c.expected {
+				t.Fatalf("expected '%s', got '%s'", c.expected, actual)
+			}
+		})
 	}
 }
 
