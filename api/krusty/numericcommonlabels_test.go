kubernetes-sigs/kustomize

commit 72c7db39f2b087dd1c75658cf580489a35279122

Migrate the label transformer.

api/builtins/LabelTransformer.go
@@ -4,9 +4,10 @@
 package builtins
 
 import (
+	"sigs.k8s.io/kustomize/api/filters/labels"
 	"sigs.k8s.io/kustomize/api/resmap"
-	"sigs.k8s.io/kustomize/api/transform"
 	"sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kyaml/filtersutil"
 	"sigs.k8s.io/yaml"
 )
 
@@ -24,14 +25,16 @@ func (p *LabelTransformerPlugin) Config(
 }
 
 func (p *LabelTransformerPlugin) Transform(m resmap.ResMap) error {
-	t, err := transform.NewMapTransformer(
-		p.FieldSpecs,
-		p.Labels,
-	)
-	if err != nil {
-		return err
+	for _, r := range m.Resources() {
+		err := filtersutil.ApplyToJSON(labels.Filter{
+			Labels:  p.Labels,
+			FsSlice: p.FieldSpecs,
+		}, r.Kunstructured)
+		if err != nil {
+			return err
+		}
 	}
-	return t.Transform(m)
+	return nil
 }
 
 func NewLabelTransformerPlugin() resmap.TransformerPlugin {

plugin/builtin/labeltransformer/LabelTransformer.go
@@ -5,9 +5,10 @@
 package main
 
 import (
+	"sigs.k8s.io/kustomize/api/filters/labels"
 	"sigs.k8s.io/kustomize/api/resmap"
-	"sigs.k8s.io/kustomize/api/transform"
 	"sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kyaml/filtersutil"
 	"sigs.k8s.io/yaml"
 )
 
@@ -28,12 +29,14 @@ func (p *plugin) Config(
 }
 
 func (p *plugin) Transform(m resmap.ResMap) error {
-	t, err := transform.NewMapTransformer(
-		p.FieldSpecs,
-		p.Labels,
-	)
-	if err != nil {
-		return err
+	for _, r := range m.Resources() {
+		err := filtersutil.ApplyToJSON(labels.Filter{
+			Labels:  p.Labels,
+			FsSlice: p.FieldSpecs,
+		}, r.Kunstructured)
+		if err != nil {
+			return err
+		}
 	}
-	return t.Transform(m)
+	return nil
 }

plugin/builtin/labeltransformer/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	sigs.k8s.io/kustomize/api v0.3.1
+	sigs.k8s.io/kustomize/kyaml v0.1.5
 	sigs.k8s.io/yaml v1.1.0
 )
 

commit 214b5bcfb9f9145d47932527cf34dc2a7abfe4d2

Test coverage for numeric-looking commonLabels strings
There is a regression in 72c7db39f2b087dd1c75658cf580489a35279122 where
commonLabels that are numeric-looking strings lose their quotation,
changing the value type from string to integer. This will cause the
resource to fail validation.

api/krusty/numericcommonlabels_test.go
@@ -1,0 +1,51 @@
+// Copyright 2020 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package krusty_test
+
+import (
+	"testing"
+
+	kusttest_test "sigs.k8s.io/kustomize/api/testutils/kusttest"
+)
+
+// This test shows numeric-looking strings correctly handled as strings
+func TestNumericCommonLabels(t *testing.T) {
+	th := kusttest_test.MakeHarness(t)
+
+	// A basic deployment just used to put labels into
+	th.WriteF("/app/default/deployment.yaml", `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: the-deployment
+`)
+
+	// Combine these custom transformers in one kustomization file.
+	// This kustomization file has a string-valued commonLabel that
+	// should always be quoted to remain a string
+	th.WriteK("/app/default", `
+commonLabels:
+  version: "1"
+resources:
+- deployment.yaml
+`)
+
+	m := th.Run("/app/default", th.MakeDefaultOptions())
+	th.AssertActualEqualsExpected(m, `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    version: "1"
+  name: the-deployment
+spec:
+  selector:
+    matchLabels:
+      version: "1"
+  template:
+    metadata:
+      labels:
+        version: "1"
+`)
+}