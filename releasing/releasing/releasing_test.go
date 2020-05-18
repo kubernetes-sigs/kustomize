32ed552ea6fe293c93221a4ac1666c3b4de58ddc
Donny Xia <donnyxia@google.com>
2020-05-18 11:42:53 -0700

    List latest versions

--- releasing/releasing/releasing_test.go
+++ releasing/releasing/releasing_test.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"os"
+	"regexp"
 	"testing"
 )
 
 func TestGetModuleCurrentVersion(t *testing.T) {
-	output := getModuleCurrentVersion("test")
-	if output != "v1.0.0" {
-		t.Errorf("Unexpected output: %s", output)
+	var err error
+	pwd, err = os.Getwd()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	remote := "upstream"
+	// Check remotes
+	checkRemoteExistence(pwd, remote)
+	// Fetch latest tags from remote
+	fetchTags(pwd, remote)
+	for _, mod := range modules {
+		v := getModuleCurrentVersion(mod)
+		valid, err := regexp.MatchString("^v(\\d+\\.){2}\\d+$", v)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		if !valid {
+			t.Errorf("Returned version %s is not valid", v)
+		}
 	}
 }
