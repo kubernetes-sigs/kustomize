d35b64ffbf6790efa2c3fe0d651e248234078c57
abkaskari <abdelhak99jaafari@gmail.com>
2020-05-18 11:42:53 -0700

    List latest versions

--- releasing/releasing/releasing.go
+++ releasing/releasing/releasing.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -11,22 +19,27 @@ var modules = [...]string{
 	"kyaml", "api", "kstatus", "cmd/config",
 	"cmd/resource", "cmd/kubectl", "pluginator", "kustomize",
 }
-var verbose bool
+var verbose bool   // Enable verbose or not
+var tempDir string // Temporary directory path for git worktree
+var pwd string     // Current working directory
 
 // === Log helper functions ===
-func logDebug(s string) {
+
+func logDebug(format string, v ...interface{}) {
 	if verbose {
-		log.Println("DEBUG " + s)
+		log.Printf("DEBUG "+format, v...)
 	}
 }
 
-func logInfo(s string) {
-	log.Println("INFO " + s)
+func logInfo(format string, v ...interface{}) {
+	log.Printf("INFO "+format, v...)
 }
 
-func logFatal(s string) {
-	log.Fatalln("FATAL " + s)
+func logFatal(format string, v ...interface{}) {
+	log.Fatalf("FATAL "+format, v...)
 }
+
+// === Command line commands ===
 
 var rootCmd = &cobra.Command{
 	Use:   "releasing",
@@ -37,7 +50,18 @@ var listSubCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List current version of all covered modules",
 	Run: func(cmd *cobra.Command, args []string) {
-		res := []string{}
+		var err error
+		pwd, err = os.Getwd()
+		if err != nil {
+			logFatal(err.Error())
+		}
+		logDebug("Working directory: %s", pwd)
+		remote := "upstream"
+		// Check remotes
+		checkRemoteExistence(pwd, remote)
+		// Fetch latest tags from remote
+		fetchTags(pwd, remote)
+		res := []string{} // Store result strings
 		for _, mod := range modules {
 			res = append(res, fmt.Sprintf("%s/%s", mod, getModuleCurrentVersion(mod)))
 		}
@@ -47,9 +71,28 @@ var listSubCmd = &cobra.Command{
 	},
 }
 
+var release = &cobra.Command{
+	Use:   "release",
+	Short: "Release a new version of specified module",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		logDebug("Preparing Git environemnt")
+		prepareGit()
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		logInfo("Done")
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		logDebug("Cleaning Git environment")
+		cleanGit()
+	},
+}
+
 var subCmds = [...]*cobra.Command{
 	listSubCmd,
+	release,
 }
+
+// === Main function ===
 
 func main() {
 	for _, cmd := range subCmds {
@@ -63,25 +106,147 @@ func main() {
 }
 
 func getModuleCurrentVersion(modName string) string {
-	mod := newModule(modName)
+	mod := newModule(modName, pwd)
 	mod.updateCurrentVersion()
-	return mod.currentVersion
+	v := mod.version.toString()
+	logDebug("module %s version.toString => %s", mod.name, v)
+	return v
 }
+
+func checkRemoteExistence(path string, remote string) {
+	logDebug("Checking remote %s in %s", remote, path)
+	cmd := exec.Command("git", "remote")
+	cmd.Dir = path
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		logFatal(err.Error())
+	}
+	logDebug("Remotes:\n%s", out.String())
+
+	regString := fmt.Sprintf("(?m)^%s$", remote)
+	reg := regexp.MustCompile(regString)
+	if !reg.MatchString(out.String()) {
+		logFatal("Cannot find remote named %s", remote)
+	}
+	logDebug("Remote %s exists", remote)
+}
+
+func fetchTags(path string, remote string) {
+	logDebug("Fetching latest tags")
+	cmd := exec.Command("git", "fetch", "-t", remote)
+	cmd.Dir = path
+	err := cmd.Run()
+	if err != nil {
+		logFatal(err.Error())
+	}
+	logDebug("Finished fetching")
+}
+
+// === module version struct and functions definition ===
+
+type moduleVersion struct {
+	major int
+	minor int
+	patch int
+}
+
+func (v moduleVersion) toString() string {
+	return fmt.Sprintf("v%d.%d.%d", v.major, v.minor, v.patch)
+}
+
+func (v *moduleVersion) set(major int, minor int, patch int) {
+	v.major = major
+	v.minor = minor
+	v.patch = patch
+}
+
+// === module struct and functions definition ===
 
 type module struct {
-	name           string
-	path           string
-	currentVersion string
+	name    string
+	path    string
+	version moduleVersion
 }
 
-func newModule(modName string) module {
+func newModule(modName string, path string) module {
 	mod := module{
 		name: modName,
+		path: path,
 	}
-	logDebug(fmt.Sprintf("Created module struct for %s", modName))
+	logDebug("Created module struct for %s", modName)
 	return mod
 }
 
 func (m *module) updateCurrentVersion() {
-	m.currentVersion = "v1.0.0"
+	logDebug("Getting latest tag for %s", m.name)
+	cmd := exec.Command("git", "tag", "-l")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Dir = m.path
+	err := cmd.Run()
+	if err != nil {
+		logFatal(err.Error())
+	}
+
+	// Search for module tag
+	regString := fmt.Sprintf("(?m)^%s/v(\\d+\\.){2}\\d+$", m.name)
+	reg := regexp.MustCompile(regString)
+	tagsString := reg.FindAllString(out.String(), -1)
+	logDebug("Tags for module %s:\n%s", m.name, tagsString)
+	var versions []moduleVersion
+	for _, tag := range tagsString {
+		v := tag[len(m.name)+2:]
+		vs := strings.Split(v, ".")
+		major, err := strconv.Atoi(vs[0])
+		if err != nil {
+			logFatal(err.Error())
+		}
+		minor, err := strconv.Atoi(vs[1])
+		if err != nil {
+			logFatal(err.Error())
+		}
+		patch, err := strconv.Atoi(vs[2])
+		if err != nil {
+			logFatal(err.Error())
+		}
+		versions = append(versions, moduleVersion{
+			major: major,
+			minor: minor,
+			patch: patch,
+		})
+	}
+	// Sort to find latest tag
+	sort.Slice(versions, func(i, j int) bool {
+		if versions[i].major == versions[j].major && versions[i].minor == versions[j].minor {
+			return versions[i].patch > versions[j].patch
+		} else if versions[i].major == versions[j].major {
+			return versions[i].minor > versions[j].minor
+		} else {
+			return versions[i].major > versions[j].major
+		}
+	})
+
+	m.version = versions[0]
+}
+
+// === Git environment functions ===
+
+func prepareGit() {
+	var err error
+	tempDir, err = ioutil.TempDir("", "kustomize-releases")
+	if err != nil {
+		logFatal(err.Error())
+	}
+	logDebug("Created git temp dir: " + tempDir)
+}
+
+func cleanGit() {
+	logDebug("Deleting git temp dir: " + tempDir)
+	err := os.RemoveAll(tempDir)
+	if err != nil {
+		logFatal(err.Error())
+	}
+	logDebug("Deleting done")
 }
