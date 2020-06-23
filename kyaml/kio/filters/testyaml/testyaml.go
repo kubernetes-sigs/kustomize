e9bd11caaaed1773d0e3098a6474f1d90951ce00
Jijie Wei <jijiew@google.com>
2020-06-22 22:23:28 -0700

    add test for fmt on yaml file of a list

--- kyaml/kio/filters/testyaml/testyaml.go
+++ kyaml/kio/filters/testyaml/testyaml.go
@@ -27,6 +27,67 @@ apiVersion: example.com/v1beta1
 kind: MyType2
 `)
 
+var UnformattedYaml3 = []byte(`
+apiVersion: v1
+items:
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    creationTimestamp: "2020-06-04T07:17:23Z"
+    name: kit-server-dev
+    resourceVersion: "7881"
+    selfLink: /api/v1/namespaces/kit-server-dev
+    uid: 6f8e0a69-a633-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    creationTimestamp: "2020-06-04T06:50:06Z"
+    name: kube-node-lease
+    resourceVersion: "40"
+    selfLink: /api/v1/namespaces/kube-node-lease
+    uid: 9fa5de74-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    creationTimestamp: "2020-06-04T06:50:05Z"
+    name: kube-public
+    resourceVersion: "26"
+    selfLink: /api/v1/namespaces/kube-public
+    uid: 9f6f664f-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    creationTimestamp: "2020-06-04T06:50:05Z"
+    name: kube-system
+    resourceVersion: "143"
+    selfLink: /api/v1/namespaces/kube-system
+    uid: 9f4b398b-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+kind: List
+metadata:
+  resourceVersion: ""
+  selfLink: ""
+`)
+
 var UnformattedJSON1 = []byte(`
 {
   "spec": "a",
@@ -69,3 +130,64 @@ var FormattedJSON1 = []byte(`{
   }
 }
 `)
+
+var FormattedYaml3 = []byte(`apiVersion: v1
+kind: List
+metadata:
+  resourceVersion: ""
+  selfLink: ""
+items:
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: kit-server-dev
+    creationTimestamp: "2020-06-04T07:17:23Z"
+    resourceVersion: "7881"
+    selfLink: /api/v1/namespaces/kit-server-dev
+    uid: 6f8e0a69-a633-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: kube-node-lease
+    creationTimestamp: "2020-06-04T06:50:06Z"
+    resourceVersion: "40"
+    selfLink: /api/v1/namespaces/kube-node-lease
+    uid: 9fa5de74-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: kube-public
+    creationTimestamp: "2020-06-04T06:50:05Z"
+    resourceVersion: "26"
+    selfLink: /api/v1/namespaces/kube-public
+    uid: 9f6f664f-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: kube-system
+    creationTimestamp: "2020-06-04T06:50:05Z"
+    resourceVersion: "143"
+    selfLink: /api/v1/namespaces/kube-system
+    uid: 9f4b398b-a62f-11ea-b632-42010a8000a7
+  spec:
+    finalizers:
+    - kubernetes
+  status:
+    phase: Active
+`)
+
