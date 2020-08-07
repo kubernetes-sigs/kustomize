a2693d0249e19e3312475d035060382c7e2c2c4c
Donny Xia <donnyxia@google.com>
2020-08-07 10:47:25 -0700

    fix panic when fieldspec refers an empty value

--- kyaml/yaml/fns.go
+++ kyaml/yaml/fns.go
@@ -467,6 +467,10 @@ func (s FieldSetter) Filter(rn *RNode) (*RNode, error) {
 		if err := ErrorIfInvalid(rn, yaml.ScalarNode); err != nil {
 			return rn, err
 		}
+		// value is nil or null
+		if IsMissingOrNull(s.Value) {
+			return rn, nil
+		}
 		// only apply the style if there is not an existing style
 		// or we want to override it
 		if !s.OverrideStyle || s.Value.YNode().Style == 0 {
