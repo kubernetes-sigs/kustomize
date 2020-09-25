2098ed53830c4825f6b4815e4e7d015ec0ce448d
abkaskari <abdelhak99jaafari@gmail.com>
2020-09-25 14:59:49 -0700

    fixed issue 2896

--- plugin/builtin/patchtransformer/PatchTransformer.go
+++ plugin/builtin/patchtransformer/PatchTransformer.go
@@ -143,9 +143,12 @@ func (p *plugin) applySMPatch(resource, patch *resource.Resource) error {
 	if err != nil {
 		return err
 	}
-	return filtersutil.ApplyToJSON(patchstrategicmerge.Filter{
+	n := resource.GetName()
+	err = filtersutil.ApplyToJSON(patchstrategicmerge.Filter{
 		Patch: node,
 	}, resource)
+	resource.SetName(n)
+	return err
 }
 
 // transformJson6902 applies the provided json6902 patch
