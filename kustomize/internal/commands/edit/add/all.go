a6a061215fe9b65f233a43c33649c1366fd81e9f
Mike Camp <Mike.Camp@sas.com>
2020-10-11 13:56:11 -0400

    Add kustomize edit add/remove transformer command #3053
    Similar to edit add/remove patch these commands
    add the ability to add or remove a transformer file path
    from the kustomization.yaml.
    
    Refactored the "remove resource" and new "remove transformer"
    tests into a common testRemoveCommand function to prevent
    code duplication.

--- kustomize/internal/commands/edit/add/all.go
+++ kustomize/internal/commands/edit/add/all.go
@@ -43,6 +43,9 @@ func NewCmdAdd(
 
 	# Adds one or more commonAnnotations to the kustomization
 	kustomize edit add annotation {annotationKey1:annotationValue1},{annotationKey2:annotationValue2}
+
+	# Adds a transformer configuration to the kustomization
+	kustomize edit add transformer <filepath>
 `,
 		Args: cobra.MinimumNArgs(1),
 	}
@@ -55,6 +58,7 @@ func NewCmdAdd(
 		newCmdAddBase(fSys),
 		newCmdAddLabel(fSys, ldr.Validator().MakeLabelValidator()),
 		newCmdAddAnnotation(fSys, ldr.Validator().MakeAnnotationValidator()),
+		newCmdAddTransformer(fSys),
 	)
 	return c
 }
