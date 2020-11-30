f41dc62dec6bcabe677dd752f3864f7d213e7d3d
abkaskari <abdelhak99jaafari@gmail.com>
2020-11-29 16:09:32 -0800

    Patchtransformers - drop copied code, improve deletion handling.

--- plugin/builtin/patchstrategicmergetransformer/PatchStrategicMergeTransformer.go
+++ plugin/builtin/patchstrategicmergetransformer/PatchStrategicMergeTransformer.go
@@ -6,14 +6,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
-
-	"github.com/pkg/errors"
-	"sigs.k8s.io/kustomize/api/filters/patchstrategicmerge"
 	"sigs.k8s.io/kustomize/api/resmap"
 	"sigs.k8s.io/kustomize/api/resource"
 	"sigs.k8s.io/kustomize/api/types"
-	"sigs.k8s.io/kustomize/kyaml/filtersutil"
 	"sigs.k8s.io/yaml"
 )
 
@@ -77,43 +72,9 @@ func (p *plugin) Transform(m resmap.ResMap) error {
 		if err != nil {
 			return err
 		}
-		patchCopy := patch.DeepCopy()
-		patchCopy.SetName(target.GetName())
-		patchCopy.SetNamespace(target.GetNamespace())
-		patchCopy.SetGvk(target.GetGvk())
-		node, err := filtersutil.GetRNode(patchCopy)
-		if err != nil {
+		if err = m.ApplySmPatch(
+			resource.MakeIdSet([]*resource.Resource{target}), patch); err != nil {
 			return err
-		}
-		err = filtersutil.ApplyToJSON(patchstrategicmerge.Filter{
-			Patch: node,
-		}, target)
-		if err != nil {
-			// Check for an error string from UnmarshalJSON that's indicative
-			// of an object that's missing basic KRM fields, and thus may have been
-			// entirely deleted (an acceptable outcome).  This error handling should
-			// be deleted along with use of ResMap and apimachinery functions like
-			// UnmarshalJSON.
-			if !strings.Contains(err.Error(), "Object 'Kind' is missing") {
-				// Some unknown error, let it through.
-				return err
-			}
-			if !target.IsEmpty() {
-				return errors.Wrapf(
-					err, "with unexpectedly non-empty object map of size %d",
-					len(target.Map()))
-			}
-			// Fall through to handle deleted object.
-		}
-		if target.IsEmpty() {
-			// This means all fields have been removed from the object.
-			// This can happen if a patch required deletion of the
-			// entire resource (not just a part of it).  This means
-			// the overall resmap must shrink by one.
-			err = m.Remove(target.CurId())
-			if err != nil {
-				return err
-			}
 		}
 	}
 	return nil
--- plugin/builtin/patchstrategicmergetransformer/go.mod
+++ plugin/builtin/patchstrategicmergetransformer/go.mod
@@ -3,9 +3,7 @@ module sigs.k8s.io/kustomize/plugin/builtin/patchstrategicmergetransformer
 go 1.15
 
 require (
-	github.com/pkg/errors v0.8.1
 	sigs.k8s.io/kustomize/api v0.6.5
-	sigs.k8s.io/kustomize/kyaml v0.9.4
 	sigs.k8s.io/yaml v1.2.0
 )
 
