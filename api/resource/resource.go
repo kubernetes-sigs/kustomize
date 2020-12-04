8154c0e9ee0fcef3037ddd8d311385024d2dc276
abkaskari <abdelhak99jaafari@gmail.com>
2020-12-04 12:29:39 -0800

    Spelling nits.

--- api/resource/resource.go
+++ api/resource/resource.go
@@ -1,7 +1,6 @@
 // Copyright 2019 The Kubernetes Authors.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package resource implements representations of k8s API resources.
 package resource
 
 import (
