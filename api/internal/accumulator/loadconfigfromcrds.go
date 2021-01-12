391b81932200410261ecdedb0835c18c31109330
uid09552 <max.rgbg@gmail.com>
2021-01-12 17:02:29 +0100

    Update loadconfigfromcrds.go
    Type definition is creating a new distinct type, which causes Type errors, changed to Alias

--- api/internal/accumulator/loadconfigfromcrds.go
+++ api/internal/accumulator/loadconfigfromcrds.go
@@ -18,7 +18,7 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-type myProperties map[string]spec.Schema
+type myProperties = map[string]spec.Schema
 type nameToApiMap map[string]common.OpenAPIDefinition
 
 // LoadConfigFromCRDs parse CRD schemas from paths into a TransformerConfig
