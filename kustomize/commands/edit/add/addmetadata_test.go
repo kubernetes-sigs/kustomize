a23896d0cc3dcd43085a419476c362e43a02eee2
abkaskari <abdelhak99jaafari@gmail.com>
2021-02-04 08:35:01 -0800

    Expose some top level kustomize commands.
    The PR exposes some of the top level kustomize commands
    (especially `build`) for reuse in other command line tools
    (expecially `kubectl`, see #1500).
    
    This PR represents option 3 from the following list of ways
    this exposure could be arranged.
    
    1. Expose the commands in the `api` module.
    
    ```
    REPO/api/go.mod
    REPO/api/builtins
    REPO/api/commands <- new
    REPO/api/...
    ```
    
    Disadvantage: This would make `api` module depend on cobra.
    That's bad for clients that want to depend on the api, but
    want to write their own commands at their own version of
    cobra.  The `api` module shouldn't depend on UX libraries
    like cobra.
    
    2. Expose the commands in their own `commands` module.
    
    They'd appear alongside `api`, e.g. `
    
    ```
    REPO/api/go.mod
    REPO/api/builtins
    REPO/api/...
    REPO/commands/go.mod
    REPO/commands/build
    REPO/commands/edit
    REPO/commands/...
    ```
    
    Advantage: The commands would be consumed by the kustomize
    binary and the kubectl binary in the same way.
    
    Disadvantage: The kustomize binary module and the commands
    module could evolve separately with their own version
    numbers, creating confusion.
    
    3. Expose the commands in the existing `kustomize` module
    
    ```
    REPO/api/go.mod
    REPO/api/builtins
    REPO/api/...
    REPO/kustomize/go.mod
    REPO/kustomize/main.go
    REPO/kustomize/commands/build
    REPO/kustomize/commands/edit
    REPO/kustomize/commands/...
    ```
    
    Outside users, e.g. kubectl, could then
    
    ```
    import sigs.k8s.io/kustomize/kustomize/v3/commands/build
    ```
    
    and hopefully still get the `main` package
    as they do now via:
    
    ```
    go get sigs.k8s.io/kustomize/kustomize/v3
    ```
    
    Advantage: 1) The kustomize binary ships at the same version
    as the commands - which makes sense as the binary's
    _version_ refers to how the CLI operates (command names,
    flags, etc.).  This makes it easy to related the version of
    a kustomize binary with the version of commands running in
    some other CLI binary.  2) The path to the kustomize binary
    doesn't change.
    
    Disadvantage: It's an atypical Go module arrangement.
    Usually `main` packages live as leaves under a directory
    called `cmd` inside a module, rather than at the _top_ of
    the module.  This might cause some problems.  If so, we can
    go with option 4.
    
    4. Same as 3, but move `main.go` (the `main` package) down one step.
    
    ```
    REPO/api/go.mod
    REPO/api/builtins
    REPO/api/...
    REPO/kustomize/go.mod
    REPO/kustomize/cmd/main.go
    REPO/kustomize/commands/build
    REPO/kustomize/commands/edit
    REPO/kustomize/commands/...
    ```

---
+++ kustomize/commands/edit/add/addmetadata_test.go
@@ -0,0 +1,369 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package add
+
+import (
+	"strings"
+	"testing"
+
+	"sigs.k8s.io/kustomize/api/filesys"
+	valtest_test "sigs.k8s.io/kustomize/api/testutils/valtest"
+	"sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/internal/kustfile"
+	testutils_test "sigs.k8s.io/kustomize/kustomize/v3/commands/internal/testutils"
+)
+
+func makeKustomization(t *testing.T) *types.Kustomization {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	kf, err := kustfile.NewKustomizationFile(fSys)
+	if err != nil {
+		t.Errorf("unexpected new error %v", err)
+	}
+	m, err := kf.Read()
+	if err != nil {
+		t.Errorf("unexpected read error %v", err)
+	}
+	return m
+}
+
+func TestRunAddAnnotation(t *testing.T) {
+	var o addMetadataOptions
+	o.metadata = map[string]string{"owls": "cute", "otters": "adorable"}
+
+	m := makeKustomization(t)
+	err := o.addAnnotations(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+	// adding the same test input should not work
+	err = o.addAnnotations(m)
+	if err == nil {
+		t.Errorf("expected already in kustomization file error")
+	}
+	// adding new annotations should work
+	o.metadata = map[string]string{"new": "annotation"}
+	err = o.addAnnotations(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+}
+
+func TestAddAnnotationNoArgs(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	err := cmd.Execute()
+	v.VerifyNoCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "must specify annotation" {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationInvalidFormat(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeSadMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"whatever:whatever"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != valtest_test.SAD {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationManyArgs(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"k1:v1,k2:v2,k3:v3,k4:v5"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationValueQuoted(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"k1:\"v1\""}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationValueWithColon(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"k1:\"v1:v2\""}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationValueWithComma(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	value := "{\"k1\":\"v1\",\"k2\":\"v2\"}"
+	args := []string{"test:" + value}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+	b, err := fSys.ReadFile("/kustomization.yaml")
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+	if !strings.Contains(string(b), value) {
+		t.Errorf(
+			"Modified file doesn't contain expected string.\nExpected string:\n%s\nActual:\n%s",
+			value, b)
+	}
+}
+
+func TestAddAnnotationNoKey(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{":nokey"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyNoCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "invalid annotation: ':nokey' (need k:v pair where v may be quoted)" {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationTooManyColons(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"key:v1:v2"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationNoValue(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"no:,value"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationMultipleArgs(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"this:annotation", "has:spaces"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddAnnotationForce(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddAnnotation(fSys, v.Validator)
+	args := []string{"key:foo"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+	// trying to add the same annotation again should not work
+	args = []string{"key:bar"}
+	v = valtest_test.MakeHappyMapValidator(t)
+	cmd = newCmdAddAnnotation(fSys, v.Validator)
+	err = cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "annotation key already in kustomization file" {
+		t.Errorf("expected an error")
+	}
+	// but trying to add it with --force should
+	v = valtest_test.MakeHappyMapValidator(t)
+	cmd = newCmdAddAnnotation(fSys, v.Validator)
+	cmd.Flag("force").Value.Set("true")
+	err = cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestRunAddLabel(t *testing.T) {
+	var o addMetadataOptions
+	o.metadata = map[string]string{"owls": "cute", "otters": "adorable"}
+
+	m := makeKustomization(t)
+	err := o.addLabels(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+	// adding the same test input should not work
+	err = o.addLabels(m)
+	if err == nil {
+		t.Errorf("expected already in kustomization file error")
+	}
+	// adding new labels should work
+	o.metadata = map[string]string{"new": "label"}
+	err = o.addLabels(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+}
+
+func TestAddLabelNoArgs(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	err := cmd.Execute()
+	v.VerifyNoCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "must specify label" {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddLabelInvalidFormat(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeSadMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{"exclamation!:point"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != valtest_test.SAD {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddLabelNoKey(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{":nokey"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyNoCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "invalid label: ':nokey' (need k:v pair where v may be quoted)" {
+		t.Errorf("incorrect error: %v", err.Error())
+	}
+}
+
+func TestAddLabelTooManyColons(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{"key:v1:v2"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddLabelNoValue(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{"no,value:"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddLabelMultipleArgs(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{"this:input", "has:spaces"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
+
+func TestAddLabelForce(t *testing.T) {
+	fSys := filesys.MakeFsInMemory()
+	testutils_test.WriteTestKustomization(fSys)
+	v := valtest_test.MakeHappyMapValidator(t)
+	cmd := newCmdAddLabel(fSys, v.Validator)
+	args := []string{"key:foo"}
+	err := cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+	// trying to add the same label again should not work
+	args = []string{"key:bar"}
+	v = valtest_test.MakeHappyMapValidator(t)
+	cmd = newCmdAddLabel(fSys, v.Validator)
+	err = cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+	if err.Error() != "label key already in kustomization file" {
+		t.Errorf("expected an error")
+	}
+	// but trying to add it with --force should
+	v = valtest_test.MakeHappyMapValidator(t)
+	cmd = newCmdAddLabel(fSys, v.Validator)
+	cmd.Flag("force").Value.Set("true")
+	err = cmd.RunE(cmd, args)
+	v.VerifyCall()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err.Error())
+	}
+}
---
+++ kustomize/commands/edit/all.go
@@ -0,0 +1,53 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package edit
+
+import (
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/kustomize/api/filesys"
+	"sigs.k8s.io/kustomize/api/ifc"
+	"sigs.k8s.io/kustomize/api/kv"
+	"sigs.k8s.io/kustomize/api/loader"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/edit/add"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/edit/fix"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/edit/listbuiltin"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/edit/remove"
+	"sigs.k8s.io/kustomize/kustomize/v3/commands/edit/set"
+)
+
+// NewCmdEdit returns an instance of 'edit' subcommand.
+func NewCmdEdit(
+	fSys filesys.FileSystem, v ifc.Validator, kf ifc.KunstructuredFactory) *cobra.Command {
+	c := &cobra.Command{
+		Use:   "edit",
+		Short: "Edits a kustomization file",
+		Long:  "",
+		Example: `
+	# Adds a configmap to the kustomization file
+	kustomize edit add configmap NAME --from-literal=k=v
+
+	# Sets the nameprefix field
+	kustomize edit set nameprefix <prefix-value>
+
+	# Sets the namesuffix field
+	kustomize edit set namesuffix <suffix-value>
+`,
+		Args: cobra.MinimumNArgs(1),
+	}
+
+	c.AddCommand(
+		add.NewCmdAdd(
+			fSys,
+			kv.NewLoader(loader.NewFileLoaderAtCwd(fSys), v),
+			kf),
+		set.NewCmdSet(
+			fSys,
+			kv.NewLoader(loader.NewFileLoaderAtCwd(fSys), v),
+			v),
+		fix.NewCmdFix(fSys),
+		remove.NewCmdRemove(fSys, v),
+		listbuiltin.NewCmdListBuiltinPlugin(),
+	)
+	return c
+}
