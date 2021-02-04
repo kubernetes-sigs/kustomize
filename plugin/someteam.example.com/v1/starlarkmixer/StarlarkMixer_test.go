f927cf0b8eabef4fa821f8e08d75141778260fc4
Katrina Verey <kn.verey@gmail.com>
2021-02-03 17:32:06 -0800

    Regression test for inability to add resources in function-based transfomer

---
+++ plugin/someteam.example.com/v1/starlarkmixer/StarlarkMixer_test.go
@@ -0,0 +1,49 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package starlarkmixer_test
+
+import (
+	"testing"
+
+	kusttest_test "sigs.k8s.io/kustomize/api/testutils/kusttest"
+)
+
+func TestStarlarkMixerPlugin(t *testing.T) {
+	th := kusttest_test.MakeEnhancedHarness(t)
+	defer th.Reset()
+
+	rm := th.LoadAndRunTransformer(`
+apiVersion: someteam.example.com/v1
+kind: StarlarkMixer
+metadata:
+  name: mixer-instance
+  annotations:
+    config.kubernetes.io/function: |
+      starlark:
+        path: mixer.star
+`,
+		`apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: some-cm
+data:
+  foo: bar
+`)
+
+	th.AssertActualEqualsExpected(rm, `
+apiVersion: v1
+data:
+  foo: bar
+kind: ConfigMap
+metadata:
+  annotations:
+    modified-by: mixer-instance
+  name: some-cm
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: net-new
+`)
+}
