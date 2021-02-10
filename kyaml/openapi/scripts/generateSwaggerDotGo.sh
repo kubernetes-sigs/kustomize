4f0ed11d2c8aedd2d0dc4f97c1cc025b026b861d
abkaskari <abdelhak99jaafari@gmail.com>
2021-02-10 14:07:45 +0800

    kyaml: set proper GOBIN
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- kyaml/openapi/scripts/generateSwaggerDotGo.sh
+++ kyaml/openapi/scripts/generateSwaggerDotGo.sh
@@ -2,7 +2,8 @@
 # Copyright 2020 The Kubernetes Authors.
 # SPDX-License-Identifier: Apache-2.0
 
-MYGOBIN=$(go env GOPATH)/bin
+MYGOBIN=$(go env GOBIN)
+MYGOBIN="${MYGOBIN:-$(go env GOPATH)/bin}"
 VERSION=$1
 
 $MYGOBIN/go-bindata \
