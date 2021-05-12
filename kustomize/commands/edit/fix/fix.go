64e5ed70ce1637cb6bd12049c184fd201f9610fa
Natasha Sarkar <natashasarkar@google.com>
2021-05-12 13:33:45 -0700

    kustomize fix should copy and compare output before running

--- kustomize/commands/edit/fix/fix.go
+++ kustomize/commands/edit/fix/fix.go
@@ -4,8 +4,13 @@
 package fix
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"sigs.k8s.io/kustomize/api/filesys"
+	"sigs.k8s.io/kustomize/api/konfig"
+	"sigs.k8s.io/kustomize/kustomize/v4/commands/build"
 	"sigs.k8s.io/kustomize/kustomize/v4/commands/internal/kustfile"
 )
 
@@ -29,6 +34,30 @@ func NewCmdFix(fSys filesys.FileSystem) *cobra.Command {
 
 // RunFix runs `fix` command
 func RunFix(fSys filesys.FileSystem) error {
+	tmpdir := fSys.DeepCopy()
+	if err := DoFix(tmpdir); err != nil {
+		return err
+	}
+
+	// compare output of `kustomize build`
+	var fixedOutput bytes.Buffer
+	fixedBuildCmd := build.NewCmdBuild(tmpdir, build.MakeHelp(konfig.ProgramName, "build"), &fixedOutput)
+	fixedBuildCmd.RunE(fixedBuildCmd, nil)
+
+	var origOutput bytes.Buffer
+	origBuildCmd := build.NewCmdBuild(fSys, build.MakeHelp(konfig.ProgramName, "build"), &origOutput)
+	origBuildCmd.RunE(origBuildCmd, nil)
+
+	if fixedOutput.String() != origOutput.String() {
+		return fmt.Errorf("could not automatically fix kustomization file, build output differs")
+	}
+
+	return DoFix(fSys)
+}
+
+// DoFix does the actual fixing work of fixing the missing and
+// deprecated fields in the kustomization file
+func DoFix(fSys filesys.FileSystem) error {
 	mf, err := kustfile.NewKustomizationFile(fSys)
 	if err != nil {
 		return err
