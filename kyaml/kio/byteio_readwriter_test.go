2971588a65b2d2d7d93081e45dec8cfc496160d7
abkaskari <abdelhak99jaafari@gmail.com>
2021-07-14 15:01:53 -0700

    fix kyaml issue with multiline scalars

--- kyaml/kio/byteio_readwriter_test.go
+++ kyaml/kio/byteio_readwriter_test.go
@@ -51,8 +51,6 @@ items:
 `,
 		},
 
-		// TODO: See issue #https://github.com/kubernetes-sigs/kustomize/issues/4066
-		//  the line breaks in scalar values are lost.
 		{
 			name: "round_trip_with_multiline_scalar",
 			input: `
@@ -87,10 +85,14 @@ spec:
       description: CanonicalService is the Schema for the canonicalservices API
       properties:
         apiVersion:
-          description: 'APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
+          description: 'APIVersion defines the versioned schema of this representation
+            of an object. Servers should convert recognized schemas to the latest
+            internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
           type: string
         kind:
-          description: 'Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
+          description: 'Kind is a string value representing the REST resource this
+            object represents. Servers may infer this from the endpoint the client
+            submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
           type: string
 `,
 		},
