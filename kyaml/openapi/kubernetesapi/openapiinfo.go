75df1a54227cd470a10bc72607f8af4cd8325038
Katrina Verey <kverey@apple.com>
2021-08-10 16:08:41 -0700

    kyaml presubmit formatting

--- kyaml/openapi/kubernetesapi/openapiinfo.go
+++ kyaml/openapi/kubernetesapi/openapiinfo.go
@@ -6,13 +6,13 @@
 package kubernetesapi
 
 import (
-  "sigs.k8s.io/kustomize/kyaml/openapi/kubernetesapi/v1212"
+	"sigs.k8s.io/kustomize/kyaml/openapi/kubernetesapi/v1212"
 )
 
 const Info = "{title:Kubernetes,version:v1.21.2}"
 
-var OpenAPIMustAsset = map[string]func(string)[]byte{
-  "v1212": v1212.MustAsset,
+var OpenAPIMustAsset = map[string]func(string) []byte{
+	"v1212": v1212.MustAsset,
 }
 
 const DefaultOpenAPI = "v1212"
