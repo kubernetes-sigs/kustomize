03c89dc53db238ef5ce60c5901f1a2ced7d5f0ea
Benjamin Morehouse <ben@getperch.app>
2021-10-15 14:33:01 -0700

    prevent misc reconnecting when running multiple builds (#4)
    Signed-off-by: Benjamin Morehouse <ben@getperch.app>

--- api/builtins/VaultSecretGenerator.go
+++ api/builtins/VaultSecretGenerator.go
@@ -3,9 +3,11 @@ package builtins
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"sigs.k8s.io/kustomize/api/kv"
@@ -29,6 +31,14 @@ const (
 	VaultPasswordEnv = "VAULT_PASSWORD"
 )
 
+const (
+	// KustomizeVaultTimeStampFormat sets the format for timestamps of last known vault connection
+	KustomizeVaultTimeStampFormat = "Mon Jan _2 15:04:05 2006"
+	// KustomizeVaultDotFile is the dotfile mounted in the home dir of the machine, which leaves when
+	// the last time you connected to vault, etc
+	KustomizeVaultDotFile = ".kustomize-vault.yml"
+)
+
 // vaultClientConnectionRetries holds how many times kustomize tries to connect to vault
 const vaultClientConnectionRetries = 5
 
@@ -44,14 +54,23 @@ type VaultSecretGeneratorPlugin struct {
 	types.VaultSecretArgs
 }
 
+// vaultSecretGeneratorPluginConfig is used to configure whenever the vault secret gen plugin
+// is connecting with Vault itself. If your environment connects to vault a bunch of times, Vault will
+// refuse you. We should limit the amount of times you reconnect, and do so with a dotfile in your root
+// called $HOME/.kustomize-vault.yml
+type vaultSecretGeneratorPluginConfig struct {
+	Token     string `yaml:"token"`
+	Timestamp string `yaml:"timestamp"`
+
+	path string `yaml:"-"`
+}
+
 // Config will load in configuration variables that the plugin uses to get secrets, and also
 // initialize the connection with the vault server
 func (p *VaultSecretGeneratorPlugin) Config(h *resmap.PluginHelpers, config []byte) (err error) {
 	if secretClient == nil {
-		for try := 0; try < vaultClientConnectionRetries; try++ {
-			if err := p.connectToVault(); err != nil {
-				return fmt.Errorf("error_connecting_to_vault: %s", err)
-			}
+		if err := p.connectToVault(); err != nil {
+			return fmt.Errorf("unable_to_connect_to_vault: %s", err)
 		}
 	}
 
@@ -94,7 +113,54 @@ func (p *VaultSecretGeneratorPlugin) Generate() (resmap.ResMap, error) {
 		kv.NewLoader(p.h.Loader(), p.h.Validator()), args)
 }
 
+// persistConnectionIfPresent will persist the connection to vault in the environment if it's present
+// and returns true if so. returns false, if no connection present, err upon unexpected environment issues
+func (p *VaultSecretGeneratorPlugin) persistConnectionIfPresent() (bool, error) {
+	vaultAddr, exists := os.LookupEnv(VaultAddressEnv)
+	if !exists {
+		return false, fmt.Errorf("vault_addr_dne")
+	}
+
+	conf, err := newVaultSecretGeneratorPluginConfig()
+	if err != nil {
+		return false, fmt.Errorf("vault_secret_config: %s", err)
+	}
+	if err := conf.read(); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, fmt.Errorf("vault_secret_config_read: %s", err)
+	}
+
+	timeStamp, err := time.Parse(KustomizeVaultTimeStampFormat, conf.Timestamp)
+	if err != nil {
+		return false, fmt.Errorf("vault_time_stamp_not_parseable: %s", err)
+	}
+	if time.Now().UTC().Add(time.Hour * -1).After(timeStamp) {
+		return false, nil
+	}
+
+	secretClient, err = vault.NewClient(&vault.Config{
+		Address:    vaultAddr,
+		HttpClient: &http.Client{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		return false, fmt.Errorf("error_creating_vault_client: %s", err)
+	}
+
+	secretClient.SetToken(conf.Token)
+	return true, nil
+}
+
 func (p *VaultSecretGeneratorPlugin) connectToVault() (err error) {
+	connected, err := p.persistConnectionIfPresent()
+	if err != nil {
+		return fmt.Errorf("persisted_vault_conn_fail: %s", err)
+	}
+	if connected {
+		return nil
+	}
+
 	for try := 0; try < vaultClientConnectionRetries; try++ {
 		err = p.connect()
 		if err != nil {
@@ -102,22 +168,33 @@ func (p *VaultSecretGeneratorPlugin) connectToVault() (err error) {
 			time.Sleep(time.Second * 2)
 			continue
 		}
+		conf, err := newVaultSecretGeneratorPluginConfig()
+		if err != nil {
+			return fmt.Errorf("vault_gen_conf_err: %s", err)
+		}
+
+		conf.Token = secretClient.Token()
+		conf.Timestamp = time.Now().UTC().Format(KustomizeVaultTimeStampFormat)
+		if err := conf.write(); err != nil {
+			return fmt.Errorf("conf_write: %s", err)
+		}
 		return nil
 	}
 	return fmt.Errorf("failed_vault_connection_after_%d_retries: %s", vaultClientConnectionRetries, err)
 }
 
+// connect runs the actual process to connect to hashicorp vault
 func (p *VaultSecretGeneratorPlugin) connect() error {
-	vaultAddr := os.Getenv(VaultAddressEnv)
-	if vaultAddr == "" {
+	vaultAddr, exists := os.LookupEnv(VaultAddressEnv)
+	if !exists {
 		return errors.New("vault_address_environment_variable_not_set: VAULT_ADDR")
 	}
-	vaultUsername := os.Getenv(VaultUsernameEnv)
-	if vaultUsername == "" {
+	vaultUsername, exists := os.LookupEnv(VaultUsernameEnv)
+	if !exists {
 		return errors.New("vault_username_environment_variable_not_set: VAULT_USERNAME")
 	}
-	vaultPassword := os.Getenv(VaultPasswordEnv)
-	if vaultPassword == "" {
+	vaultPassword, exists := os.LookupEnv(VaultPasswordEnv)
+	if !exists {
 		return errors.New("vault_password_environment_variable_not_set: VAULT_PASSWORD")
 	}
 
@@ -146,7 +223,43 @@ func (p *VaultSecretGeneratorPlugin) connect() error {
 	return nil
 }
 
+func (p *vaultSecretGeneratorPluginConfig) write() error {
+	raw, err := yaml.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("yaml_marshal: %s", err)
+	}
+	if err := ioutil.WriteFile(p.path, raw, 0744); err != nil {
+		return fmt.Errorf("vault_yaml_write: %s", err)
+	}
+	return nil
+}
+
+func (p *vaultSecretGeneratorPluginConfig) read() error {
+	raw, err := ioutil.ReadFile(p.path)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("conf_read_file: %s", err)
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return os.ErrNotExist
+	}
+
+	if err := yaml.Unmarshal(raw, &p); err != nil {
+		return fmt.Errorf("file_unmarshal_err: %s", err)
+	}
+	return nil
+}
+
 // NewVaultSecretGeneratorPlugin creates a generator for vault secrets
 func NewVaultSecretGeneratorPlugin() resmap.GeneratorPlugin {
 	return &VaultSecretGeneratorPlugin{}
+}
+
+func newVaultSecretGeneratorPluginConfig() (*vaultSecretGeneratorPluginConfig, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("home_dir: %s", err)
+	}
+
+	path := filepath.Join(homeDir, KustomizeVaultDotFile)
+	return &vaultSecretGeneratorPluginConfig{path: path}, nil
 }
