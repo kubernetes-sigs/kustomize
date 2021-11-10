9c7303e1c15ae8d125c9bc32848664e3e2866bbe
Justin SB <justinsb@google.com>
2021-11-10 09:23:28 -0500

    Don't swallow error in HelmChartInflationGenerator
    I had an issue where I couldn't execute the binary, and the
    information I needed was in the error.

--- plugin/builtin/helmchartinflationgenerator/HelmChartInflationGenerator.go
+++ plugin/builtin/helmchartinflationgenerator/HelmChartInflationGenerator.go
@@ -155,8 +155,8 @@ func (p *HelmChartInflationGeneratorPlugin) runHelmCommand(
 		helm := p.h.GeneralConfig().HelmConfig.Command
 		err = errors.Wrap(
 			fmt.Errorf(
-				"unable to run: '%s %s' with env=%s (is '%s' installed?)",
-				helm, strings.Join(args, " "), env, helm),
+				"unable to run: '%s %s' with env=%s (is '%s' installed?): %w",
+				helm, strings.Join(args, " "), env, helm, err),
 			stderr.String(),
 		)
 	}
