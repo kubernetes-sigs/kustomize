a06673ec178815ea6321c4cc2a83c3a0eb19bbe3
abkaskari <abdelhak99jaafari@gmail.com>
2021-11-19 12:13:12 -0800

    add to BuildAnnotations

--- kyaml/kio/kioutil/kioutil.go
+++ kyaml/kio/kioutil/kioutil.go
@@ -41,6 +41,11 @@ const (
 
 	// Deprecated: use IdAnnotation instead.
 	LegacyIdAnnotation = "config.k8s.io/id"
+
+	// InternalAnnotationsMigrationResourceIDAnnotation is used to uniquely identify
+	// resources during round trip to and from a function execution. We will use it
+	// to track the internal annotations and reconcile them if needed.
+	InternalAnnotationsMigrationResourceIDAnnotation = "internal.config.k8s.io/annotations-migration-resource-id"
 )
 
 func GetFileAnnotations(rn *yaml.RNode) (string, string, error) {
