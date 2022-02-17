35ddd407819d1f29779ce35095b33014cb898635
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2022-02-17 17:40:47 +0100

    feat: support edit add/set label for labels and not only commonLabels

--- kustomize/commands/edit/set/setlabel_test.go
+++ kustomize/commands/edit/set/setlabel_test.go
@@ -4,6 +4,7 @@
 package set
 
 import (
+	"reflect"
 	"testing"
 
 	valtest_test "sigs.k8s.io/kustomize/api/testutils/valtest"
@@ -28,8 +29,9 @@ func makeKustomization(t *testing.T) *types.Kustomization {
 	return m
 }
 
-func TestRunSetLabel(t *testing.T) {
+func TestRunSetLabels(t *testing.T) {
 	var o setLabelOptions
+	o.includeSelectors = true
 	o.metadata = map[string]string{"owls": "cute", "otters": "adorable"}
 
 	m := makeKustomization(t)
@@ -37,6 +39,15 @@ func TestRunSetLabel(t *testing.T) {
 	if err != nil {
 		t.Errorf("unexpected error: could not write to kustomization file")
 	}
+
+	// assert content
+	expectedContent := map[string]string{"app": "helloworld", "owls": "cute", "otters": "adorable"}
+	if !reflect.DeepEqual(m.CommonLabels, expectedContent) {
+		t.Log("m.CommonLabels", m.CommonLabels)
+		t.Log("expectedContent", expectedContent)
+		t.Errorf("commonLabels does not contain expected content")
+	}
+
 	// adding the same test input should work
 	err = o.setLabels(m)
 	if err != nil {
@@ -47,6 +58,62 @@ func TestRunSetLabel(t *testing.T) {
 	err = o.setLabels(m)
 	if err != nil {
 		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+}
+
+func TestRunSetLabelsNoSelector(t *testing.T) {
+	var o setLabelOptions
+	o.includeSelectors = false
+	o.metadata = map[string]string{"owls": "cute", "otters": "adorable"}
+
+	m := makeKustomization(t)
+	err := o.setLabels(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+
+	// assert content
+	expectedContent := make([]types.Label, 2)
+	expectedContent[0] = types.Label{Pairs: map[string]string{"owls": "cute"}, IncludeSelectors: false}
+	expectedContent[1] = types.Label{Pairs: map[string]string{"otters": "adorable"}, IncludeSelectors: false}
+	if !reflect.DeepEqual(m.Labels, expectedContent) {
+		t.Log("m.Labels", m.Labels)
+		t.Log("expectedContent", expectedContent)
+		t.Errorf("labels does not contain expected content")
+	}
+
+	// adding the same test input should work
+	err = o.setLabels(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+
+	// assert content
+	expectedContent2 := make([]types.Label, 2)
+	expectedContent2[0] = types.Label{Pairs: map[string]string{"owls": "cute"}, IncludeSelectors: false}
+	expectedContent2[1] = types.Label{Pairs: map[string]string{"otters": "adorable"}, IncludeSelectors: false}
+	if !reflect.DeepEqual(m.Labels, expectedContent2) {
+		t.Log("m.Labels", m.Labels)
+		t.Log("expectedContent", expectedContent2)
+		t.Errorf("labels does not contain expected content")
+	}
+
+	// adding new labels should work
+	o.metadata = map[string]string{"new": "label", "owls": "not cute"}
+	err = o.setLabels(m)
+	if err != nil {
+		t.Errorf("unexpected error: could not write to kustomization file")
+	}
+
+	// assert content
+	expectedContent3 := make([]types.Label, 3)
+	expectedContent3[0] = types.Label{Pairs: map[string]string{"owls": "not cute"}, IncludeSelectors: false}
+	expectedContent3[1] = types.Label{Pairs: map[string]string{"otters": "adorable"}, IncludeSelectors: false}
+	expectedContent3[2] = types.Label{Pairs: map[string]string{"new": "label"}, IncludeSelectors: false}
+	if !reflect.DeepEqual(m.Labels, expectedContent3) {
+		t.Log("m.Labels", m.Labels)
+		t.Log("expectedContent", expectedContent3)
+		t.Errorf("labels does not contain expected content")
 	}
 }
 
