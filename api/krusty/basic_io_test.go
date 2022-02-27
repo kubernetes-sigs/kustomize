e038e0d2ed83c50b750ba9f830bdf320c45c76fd
abkaskari <abdelhak99jaafari@gmail.com>
2022-02-27 08:11:42 +0000

    test case added

--- api/krusty/basic_io_test.go
+++ api/krusty/basic_io_test.go
@@ -6,6 +6,7 @@ package krusty_test
 import (
 	"testing"
 
+	"sigs.k8s.io/kustomize/api/internal/kusterr"
 	kusttest_test "sigs.k8s.io/kustomize/api/testutils/kusttest"
 )
 
@@ -79,4 +80,36 @@ metadata:
 spec:
   clusterIP: None
 `)
+}
+
+//test for https://github.com/kubernetes-sigs/kustomize/issues/3812#issuecomment-862339267
+func TestBasicIO3812(t *testing.T) {
+	th := kusttest_test.MakeHarness(t)
+	th.WriteK(".", `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - service.yaml
+`)
+
+	th.WriteF("service.yaml", `
+apiVersion: v1
+kind: Service
+metadata:
+  name: kapacitor
+  labels:
+    app.kubernetes.io/name: tick-kapacitor
+spec:
+  selector:
+    app.kubernetes.io/name: tick-kapacitor
+    - name: http
+      port: 9092
+      protocol: TCP
+  type: ClusterIP
+`)
+
+	err := th.RunWithErr(".", th.MakeDefaultOptions())
+	if !kusterr.IsMalformedYAMLError(err) {
+		t.Fatalf("unexpected error: %q", err)
+	}
 }
