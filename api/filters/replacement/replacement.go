2f2e14e953c8291f4a4aade09e355fb5eab10612
koba1t <kobdotsh@gmail.com>
2022-04-13 12:43:35 +0900

    fix lint error to nolint:goerr113

--- api/filters/replacement/replacement.go
+++ api/filters/replacement/replacement.go
@@ -123,7 +123,7 @@ func applyToNode(node *yaml.RNode, value *yaml.RNode, target *types.TargetSelect
 			// So, if create option is set, we fallback to PathGetter.
 			for _, f := range fieldPath {
 				if f == "*" {
-					return errors.New("cannot support create option in a multi-value target")
+					return errors.New("cannot support create option in a multi-value target") //nolint:goerr113
 				}
 			}
 			t, err = node.Pipe(yaml.LookupCreate(value.YNode().Kind, fieldPath...))
