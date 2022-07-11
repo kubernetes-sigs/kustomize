a0e3137de1a9420af56e3f4d682576151ff5e980
abkaskari <abdelhak99jaafari@gmail.com>
2022-07-11 10:34:08 -0700

    update release tools for go workspace mode

--- cmd/gorepomod/internal/edit/editor.go
+++ cmd/gorepomod/internal/edit/editor.go
@@ -57,8 +57,6 @@ func (e *Editor) Tidy() error {
 func (e *Editor) Pin(target misc.LaModule, oldV, newV semver.SemVer) error {
 	err := e.run(
 		"edit",
-		"-dropreplace="+target.ImportPath(),
-		"-dropreplace="+target.ImportPath()+"@"+oldV.String(),
 		"-require="+target.ImportPath()+"@"+newV.String(),
 	)
 	if err != nil {
