15545cc228f5531a37d29f302a5e205628974eee
Agustina Barbetta <agustina.barbetta@gmail.com>
2022-08-07 20:48:51 -0300

    create template/metadata in includeTemplates if not present

--- api/internal/target/kusttarget_configplugin.go
+++ api/internal/target/kusttarget_configplugin.go
@@ -288,7 +288,7 @@ var transformerConfigurators = map[builtinhelpers.BuiltinPluginType]func(
 			} else {
 				// merge spec/template/metadata fieldSpec if includeTemplate flag is true
 				if label.IncludeTemplates {
-					fss, err = fss.MergeOne(types.FieldSpec{Path: "spec/template/metadata/labels", CreateIfNotPresent: false})
+					fss, err = fss.MergeOne(types.FieldSpec{Path: "spec/template/metadata/labels", CreateIfNotPresent: true})
 					if err != nil {
 						return nil, errors.Wrap(err, "failed to merge template fieldSpec")
 					}
