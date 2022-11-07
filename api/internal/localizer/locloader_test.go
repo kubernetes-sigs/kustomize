077f547c5a75437e9793d7d42fe7b258702d09ca
Anna Song <annasong@google.com>
2022-11-07 14:21:53 -0800

    Add localizer unit tests

--- api/internal/localizer/locloader_test.go
+++ api/internal/localizer/locloader_test.go
@@ -16,22 +16,7 @@ import (
 
 const dstPrefix = "localized"
 
-func makeMemoryFs(t *testing.T) filesys.FileSystem {
-	t.Helper()
-	req := require.New(t)
-
-	fSys := filesys.MakeFsInMemory()
-	req.NoError(fSys.MkdirAll("/a/b"))
-	req.NoError(fSys.WriteFile("/a/kustomization.yaml", []byte("/a")))
-
-	dirChain := "/alpha/beta/gamma/delta"
-	req.NoError(fSys.MkdirAll(dirChain))
-	req.NoError(fSys.WriteFile(dirChain+"/kustomization.yaml", []byte(dirChain)))
-	req.NoError(fSys.Mkdir("/alpha/beta/c"))
-	return fSys
-}
-
-func checkNewLocLoader(req *require.Assertions, ldr ifc.Loader, args *lclzr.LocArgs, target string, scope string, newDir string, fSys filesys.FileSystem) {
+func checkNewLocLoader(req *require.Assertions, ldr *lclzr.LocLoader, args *lclzr.LocArgs, target string, scope string, newDir string, fSys filesys.FileSystem) {
 	checkLoader(req, ldr, target)
 	checkLocArgs(req, args, target, scope, newDir, fSys)
 }
@@ -64,9 +49,9 @@ func TestLocalLoadNewAndCleanup(t *testing.T) {
 	req.Equal(fSysCopy, fSys)
 
 	// easy load directly in root
-	content, err := ldr.Load("kustomization.yaml")
+	content, err := ldr.Load("pod.yaml")
 	req.NoError(err)
-	req.Equal([]byte("/a"), content)
+	req.Equal([]byte("pod configuration"), content)
 
 	// typical sibling root reference
 	sibLdr, err := ldr.New("../alpha")
@@ -107,18 +92,18 @@ func TestNewLocLoaderDefaultForRootTarget(t *testing.T) {
 			checkNewLocLoader(req, ldr, &args, "/", "/", "/"+dstPrefix, fSys)
 
 			// file in root, but nested
-			content, err := ldr.Load("a/kustomization.yaml")
+			content, err := ldr.Load("a/pod.yaml")
 			req.NoError(err)
-			req.Equal([]byte("/a"), content)
+			req.Equal([]byte("pod configuration"), content)
 
 			childLdr, err := ldr.New("a")
 			req.NoError(err)
 			checkLoader(req, childLdr, "/a")
 
 			// messy, uncleaned path
-			content, err = childLdr.Load("./../a/kustomization.yaml")
+			content, err = childLdr.Load("./../a/pod.yaml")
 			req.NoError(err)
-			req.Equal([]byte("/a"), content)
+			req.Equal([]byte("pod configuration"), content)
 		})
 	}
 }
@@ -139,9 +124,9 @@ func TestNewMultiple(t *testing.T) {
 	checkLoader(req, descLdr, "/alpha/beta/gamma/delta")
 
 	// upwards traversal
-	higherLdr, err := descLdr.New("../../c")
+	higherLdr, err := descLdr.New("../../say")
 	req.NoError(err)
-	checkLoader(req, higherLdr, "/alpha/beta/c")
+	checkLoader(req, higherLdr, "/alpha/beta/say")
 }
 
 func makeWdFs(t *testing.T) map[string]filesys.FileSystem {
@@ -215,7 +200,7 @@ func TestNewLocLoaderFails(t *testing.T) {
 			"/newDir",
 		},
 		"file target": {
-			"/a/kustomization.yaml",
+			"/a/pod.yaml",
 			"/",
 			"/newDir",
 		},
@@ -260,7 +245,7 @@ func TestNewFails(t *testing.T) {
 		"at dst":            "newDir",
 		"ancestor":          "../../beta",
 		"non-existent root": "delt",
-		"file":              "delta/kustomization.yaml",
+		"file":              "delta/deployment.yaml",
 	}
 	for name, root := range cases {
 		root := root
@@ -285,11 +270,11 @@ func TestLoadFails(t *testing.T) {
 	checkNewLocLoader(req, ldr, &args, "/a", "/a", "/a/newDir", fSys)
 
 	cases := map[string]string{
-		"absolute path":     "/a/kustomization.yaml",
+		"absolute path":     "/a/pod.yaml",
 		"directory":         "b",
 		"non-existent file": "kubectl.yaml",
-		"file outside root": "../alpha/beta/gamma/delta/kustomization.yaml",
-		"inside dst":        "newDir/kustomization.yaml",
+		"file outside root": "../alpha/beta/gamma/delta/deployment.yaml",
+		"inside dst":        "newDir/pod.yaml",
 	}
 	for name, file := range cases {
 		file := file
@@ -300,7 +285,7 @@ func TestLoadFails(t *testing.T) {
 			ldr, _, err := lclzr.NewLocLoader("./a/../a", "/a/../a", "/a/newDir", fSys)
 			req.NoError(err)
 
-			req.NoError(fSys.WriteFile("/a/newDir/kustomization.yaml", []byte("/a/newDir")))
+			req.NoError(fSys.WriteFile("/a/newDir/pod.yaml", []byte("pod configuration")))
 
 			_, err = ldr.Load(file)
 			req.Error(err)
