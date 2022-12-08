8a6602db94934e9ec10990339c2ff7fc142d6215
Christopher Fry <christopherfry@google.com>
2022-12-08 20:10:05 +0000

    Allow name suffix transformer configuration to be overridden

--- api/internal/plugins/builtinconfig/transformerconfig.go
+++ api/internal/plugins/builtinconfig/transformerconfig.go
@@ -60,6 +60,7 @@ func MakeTransformerConfig(
 // sortFields provides determinism in logging, tests, etc.
 func (t *TransformerConfig) sortFields() {
 	sort.Sort(t.NamePrefix)
+	sort.Sort(t.NameSuffix)
 	sort.Sort(t.NameSpace)
 	sort.Sort(t.CommonLabels)
 	sort.Sort(t.TemplateLabels)
