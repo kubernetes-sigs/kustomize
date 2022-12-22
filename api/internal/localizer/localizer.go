4b4f1b11e0fd0957c92beffd518d25143d5e5a43
Anna Song <annasong@google.com>
2022-12-22 15:15:26 -0500

    Implement locRootPath (#4909)
    * Implement locRootPath, and include userinfo, port in locFilePath
    
    * Strip userinfo, port
    
    * Improve readability

--- api/internal/localizer/localizer.go
+++ api/internal/localizer/localizer.go
@@ -296,7 +296,10 @@ func (lc *localizer) localizeDir(path string) (string, error) {
 	if repo := ldr.Repo(); repo != "" {
 		// TODO(annasong): You need to check if you can add a localize directory here to store
 		// the remote file content. There may be a directory that shares the localize directory name.
-		locPath = locRootPath(path, repo, root)
+		locPath, err = locRootPath(path, repo, root, lc.fSys)
+		if err != nil {
+			return "", err
+		}
 	} else {
 		locPath, err = filepath.Rel(lc.root.String(), root.String())
 		if err != nil {
