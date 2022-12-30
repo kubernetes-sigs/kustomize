f8a0aeb02d35bbcf439707adc6528f45dbf4c784
Matthieu MOREL <matthieu.morel35@gmail.com>
2022-12-30 12:16:09 +0000

    add npm package-ecosystem
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- Makefile-dependabot.mk
+++ Makefile-dependabot.mk
@@ -1,6 +1,8 @@
+DOCKER_DIRS := $(shell find . -type f -name "Dockerfile" -exec dirname {} \; | sort -u | egrep  '^./' )
+
 GOMOD_DIRS := $(shell find . -type f -name "go.mod" -exec dirname {} \; | sort -u | egrep  '^./' )
 
-DOCKER_DIRS := $(shell find . -type f -name "Dockerfile" -exec dirname {} \; | sort -u | egrep  '^./' )
+NPM_DIRS := $(shell find . -type f -name "package.json" -exec dirname {} \; | sort -u | egrep  '^./' )
 
 DEPENDABOT_PATH=".github/dependabot.yml"
 
@@ -21,10 +23,13 @@ generate/dependabot: $(eval SHELL:=/bin/bash)
 	@echo "" >> ${DEPENDABOT_PATH}
 	@echo "version: 2" >> ${DEPENDABOT_PATH}
 	@echo "updates:" >> ${DEPENDABOT_PATH}
-	$(MAKE) dependabot/update DIR="/" PACKAGE="github-actions"
 	@set -e; for dir in $(DOCKER_DIRS); do \
 		$(MAKE) dependabot/update DIR=$${dir:1} PACKAGE="docker"; \
 	done
+	$(MAKE) dependabot/update DIR="/" PACKAGE="github-actions"
 	@set -e; for dir in $(GOMOD_DIRS); do \
 		$(MAKE) dependabot/update DIR=$${dir:1} PACKAGE="gomod"; \
+	done
+	@set -e; for dir in $(NPM_DIRS); do \
+		$(MAKE) dependabot/update DIR=$${dir:1} PACKAGE="npm"; \
 	done
