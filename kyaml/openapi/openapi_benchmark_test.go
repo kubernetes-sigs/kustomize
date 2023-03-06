9377afeb86f2bbd521738b34a37cacd11db4c476
Ed Overton <ed.overton@sas.com>
2023-03-06 10:23:44 -0500

    test: add openapi.IsNamespaceScoped benchmark
    Add a benchmark test for IsNamespaceScoped performance when the default
    schema is in use.

--- kyaml/openapi/openapi_benchmark_test.go
+++ kyaml/openapi/openapi_benchmark_test.go
@@ -5,6 +5,7 @@ package openapi
 
 import (
 	"path/filepath"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 	"strings"
 	"testing"
 
@@ -29,5 +30,21 @@ func BenchmarkProtoUnmarshal(t *testing.B) {
 		if err := proto.Unmarshal(b, &openapi_v2.Document{}); err != nil {
 			t.Fatalf("proto.Unmarshal failed: %v", err)
 		}
+	}
+}
+
+func BenchmarkPrecomputedIsNamespaceScoped(b *testing.B) {
+	testcases := map[string]yaml.TypeMeta{
+		"namespace scoped": {APIVersion: "apps/v1", Kind: "ControllerRevision"},
+		"cluster scoped":   {APIVersion: "rbac.authorization.k8s.io/v1", Kind: "ClusterRole"},
+		"unknown resource": {APIVersion: "custom.io/v1", Kind: "Custom"},
+	}
+	for name, testcase := range testcases {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ResetOpenAPI()
+				_, _ = IsNamespaceScoped(testcase)
+			}
+		})
 	}
 }
