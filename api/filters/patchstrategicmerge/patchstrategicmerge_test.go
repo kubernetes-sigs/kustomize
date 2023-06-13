c76fd5eb852076fcbd249cb54633b867840af128
Ed Overton <ed.overton@sas.com>
2023-06-13 14:50:22 -0400

    test: update psm key style test

--- api/filters/patchstrategicmerge/patchstrategicmerge_test.go
+++ api/filters/patchstrategicmerge/patchstrategicmerge_test.go
@@ -852,7 +852,7 @@ metadata:
   name: blabla
   namespace: blabla-ns
 data:
-  "6443": "foobar"
+  "6443": "key-double-quoted"
 `,
 			patch: yaml.MustParse(`
 apiVersion: v1
@@ -861,7 +861,7 @@ metadata:
   name: blabla
   namespace: blabla-ns
 data:
-  "6443": "barfoo"
+  6443: "key as int"
   '9110': "foo-foo"
 `),
 			expected: `
@@ -871,7 +871,7 @@ metadata:
   name: blabla
   namespace: blabla-ns
 data:
-  "6443": "barfoo"
+  "6443": "key as int"
   '9110': "foo-foo"
 `,
 		},
