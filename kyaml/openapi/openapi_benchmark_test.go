ed9553bcb5b76dfb60bccb24c7a66446af00633c
abkaskari <abdelhak99jaafari@gmail.com>
2023-09-08 15:34:30 -0400

    perf: limit initSchema calls from openapi.IsNamespaceScoped  (#5076)
    * test: add openapi.IsNamespaceScoped benchmark
    
    Add a benchmark test for IsNamespaceScoped performance when the default
    schema is in use.
    
    * perf: limit initSchema calls from openapi.IsNamespaceScoped
    
    Avoid calling initSchema from openapi.IsNamespaceScoped when possible.
    Work done in #4152 introduced a precomputed namespace scope map based on
    the default built-in schema. This commit extends that work by avoiding
    calls to initSchema when a resource is not found in the precomputed map
    and the default built-in schema is in use. In those cases, there is no
    benefit to calling initSchema since the precomputed map is exactly what
    will be calculated by parsing the default built-in schema.
    
    * fix: delay parsing of default built-in schema
    
    When namespace scope can be determined by the precomputed map but the
    type is not present in the precomputed map, delay the parsing of the
    default built-in schema.
    
    If the schema to be initialized is the default built-in schema and the
    type is not in the precomputed map, then the type will not be found in
    the default built-in schema. There is no need to parse the default
    built-in schema for that answer; its parsing may be delayed until it
    is needed for some other purpose.
    
    In cases where the schema is used solely for namespace scope checks, the
    schema might not ever be parsed. Skipping the parsing reduces both
    execution time and memory use.
    
    * fix: correct openapi.go's schemaNotParsed value
    
    openapiData initializes with defaultBuiltInSchemaParseStatus set to 0,
    so schemaNotParsed should have 0 as its value.

--- kyaml/openapi/openapi_benchmark_test.go
+++ kyaml/openapi/openapi_benchmark_test.go
@@ -11,6 +11,7 @@ import (
 	openapi_v2 "github.com/google/gnostic-models/openapiv2"
 	"google.golang.org/protobuf/proto"
 	"sigs.k8s.io/kustomize/kyaml/openapi/kubernetesapi"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
 func BenchmarkProtoUnmarshal(t *testing.B) {
@@ -29,5 +30,21 @@ func BenchmarkProtoUnmarshal(t *testing.B) {
 		if err := proto.Unmarshal(b, &openapi_v2.Document{}); err != nil {
 			t.Fatalf("proto.Unmarshal failed: %v", err)
 		}
+	}
+}
+
+func BenchmarkPrecomputedIsNamespaceScoped(b *testing.B) {
+	testcases := map[string]yaml.TypeMeta{
+		"namespace scoped": {APIVersion: "apps/v1", Kind: "ControllerRevision"},
+		"cluster scoped":   {APIVersion: "rbac.authorization.k8s.io/v1", Kind: "ClusterRole"},
+		"unknown resource": {APIVersion: "custom.io/v1", Kind: "Custom"},
+	}
+	for name, testcase := range testcases {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ResetOpenAPI()
+				_, _ = IsNamespaceScoped(testcase)
+			}
+		})
 	}
 }
