3d22f210fd15f743cfb07462ea24feed45a78d65
abkaskari <abdelhak99jaafari@gmail.com>
2023-09-21 07:15:10 +0900

    fix failed tests for fn framework generator

---
+++ cmd/config/internal/commands/e2e/e2econtainersimplegenerator/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The Kubernetes Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package main contains a function to be used for e2e testing.
+// See https://github.com/kubernetes-sigs/kustomize/blob/master/cmd/config/docs/api-conventions/functions-spec.md
+package main
