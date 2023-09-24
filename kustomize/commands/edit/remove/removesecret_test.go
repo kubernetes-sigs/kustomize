a318d4db26115a1c67563a232d59e9c857efbd65
Mauren Berti <stormqueen1990@gmail.com>
2023-09-24 19:08:24 -0400

    feat: incorporate feedback from review
    * Incorporate feedback from reviews.
    * Add extra test cases to increase coverage.
    * Tiny refactors for code parity with remove configmap.
    * Update copyright notice.

--- kustomize/commands/edit/remove/removesecret_test.go
+++ kustomize/commands/edit/remove/removesecret_test.go
@@ -1,15 +1,14 @@
-// Copyright 2019 The Kubernetes Authors.
+// Copyright 2023 The Kubernetes Authors.
 // SPDX-License-Identifier: Apache-2.0
 
 package remove //nolint:testpackage
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	testutils_test "sigs.k8s.io/kustomize/kustomize/v4/commands/internal/testutils"
+	"github.com/stretchr/testify/require"
+	testutils_test "sigs.k8s.io/kustomize/kustomize/v5/commands/internal/testutils"
 	"sigs.k8s.io/kustomize/kyaml/filesys"
 )
 
@@ -18,9 +17,10 @@ func TestRemoveSecret(t *testing.T) {
 	const secretName02 = "example-secret-02"
 
 	tests := map[string]struct {
-		input       string
-		args        []string
-		expectedErr string
+		input          string
+		args           []string
+		expectedOutput string
+		expectedErr    string
 	}{
 		"happy path": {
 			input: fmt.Sprintf(`
@@ -32,6 +32,10 @@ secretGenerator:
   - longsecret.txt
 `, secretName01),
 			args: []string{secretName01},
+			expectedOutput: `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+`,
 		},
 		"multiple": {
 			input: fmt.Sprintf(`
@@ -48,6 +52,10 @@ secretGenerator:
 			args: []string{
 				fmt.Sprintf("%s,%s", secretName01, secretName02),
 			},
+			expectedOutput: `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+`,
 		},
 		"miss": {
 			input: fmt.Sprintf(`
@@ -58,7 +66,31 @@ secretGenerator:
   files:
   - longsecret.txt
 `, secretName01),
-			args: []string{"foo"},
+			args:        []string{"foo"},
+			expectedErr: "no specified secret(s) were found",
+		},
+		"no secret name specified": {
+			args:        []string{},
+			expectedErr: "at least one secret name must be specified",
+		},
+		"too many secret names specified": {
+			args:        []string{"test1", "test2"},
+			expectedErr: "too many arguments",
+		},
+		"one existing and one non-existing": {
+			input: fmt.Sprintf(`
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+secretGenerator:
+- name: %s
+  files:
+  - application.properties
+`, secretName01),
+			args: []string{fmt.Sprintf("%s,%s", secretName01, "foo")},
+			expectedOutput: `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+`,
 		},
 	}
 
@@ -68,17 +100,17 @@ secretGenerator:
 			testutils_test.WriteTestKustomizationWith(fSys, []byte(tc.input))
 			cmd := newCmdRemoveSecret(fSys)
 			err := cmd.RunE(cmd, tc.args)
+
 			if tc.expectedErr != "" {
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), tc.expectedErr)
-			} else {
-				assert.NoError(t, err)
-				content, err := testutils_test.ReadTestKustomization(fSys)
-				assert.NoError(t, err)
-				for _, opt := range strings.Split(tc.args[0], ",") {
-					assert.NotContains(t, string(content), opt)
-				}
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expectedErr)
+				return
 			}
+
+			require.NoError(t, err)
+			content, err := testutils_test.ReadTestKustomization(fSys)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedOutput, string(content))
 		})
 	}
 }
