21bcbc699e4dc355c2e286bf6e4b55face53ca90
Nick Capps <nickcapps10@gmail.com>
2023-11-24 17:11:56 -0800

    Add commonLabels deprecation warning message

--- api/types/kustomization.go
+++ api/types/kustomization.go
@@ -188,6 +188,7 @@ const (
 	deprecatedPatchesJson6902Message           = "# Warning: 'patchesJson6902' is deprecated. Please use 'patches' instead." + " " + deprecatedWarningToRunEditFix
 	deprecatedPatchesStrategicMergeMessage     = "# Warning: 'patchesStrategicMerge' is deprecated. Please use 'patches' instead." + " " + deprecatedWarningToRunEditFix
 	deprecatedVarsMessage                      = "# Warning: 'vars' is deprecated. Please use 'replacements' instead." + " " + deprecatedWarningToRunEditFixExperimential
+	deprecatedCommonLabelsWarningMessage       = "# Warning: 'commonLabels' is deprecated. Please use 'labels' instead."
 )
 
 // CheckDeprecatedFields check deprecated field is used or not.
@@ -207,6 +208,9 @@ func (k *Kustomization) CheckDeprecatedFields() *[]string {
 	}
 	if k.Vars != nil {
 		warningMessages = append(warningMessages, deprecatedVarsMessage)
+	}
+	if k.CommonLabels != nil {
+		warningMessages = append(warningMessages, deprecatedCommonLabelsWarningMessage)
 	}
 	return &warningMessages
 }
