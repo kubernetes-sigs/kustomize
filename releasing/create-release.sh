4a813b48902fb28d086288447a79a5e1bff34447
Kurnianto Trilaksono <antokurnianto82@gmail.com>
2024-02-04 09:48:05 +0800

    split build-kustomize-libarry from create-release

--- releasing/create-release.sh
+++ releasing/create-release.sh
@@ -35,54 +35,6 @@ elif [[ ! "${RELEASE_TYPES[*]}" =~ "${2}" ]]; then
   exit 1
 fi
 
-# Build the release binaries for every OS/arch combination.
-# It builds compressed artifacts on $release_dir.
-function build_kustomize_binary {
-  echo "build kustomize binaries"
-  version=$1
-
-  release_dir=$2
-  echo "build release artifacts to $release_dir"
-
-  mkdir -p "output"
-  # build date in ISO8601 format
-  build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-  for os in linux darwin windows; do
-    arch_list=(amd64 arm64)
-    if [ "$os" == "linux" ]; then
-      arch_list=(amd64 arm64 s390x ppc64le)
-    fi
-    for arch in "${arch_list[@]}" ; do
-      echo "Building $os-$arch"
-    #   CGO_ENABLED=0 GOWORK=off GOOS=$os GOARCH=$arch go build -o output/kustomize -ldflags\
-      binary_name="kustomize"
-      [[ "$os" == "windows" ]] && binary_name="kustomize.exe"
-      CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o output/$binary_name -ldflags\
-        "-s -w\
-        -X sigs.k8s.io/kustomize/api/provenance.version=$version\
-        -X sigs.k8s.io/kustomize/api/provenance.gitCommit=$(git rev-parse HEAD)\
-        -X sigs.k8s.io/kustomize/api/provenance.buildDate=$build_date"\
-        kustomize/main.go
-      if [ "$os" == "windows" ]; then
-        zip -j "${release_dir}/kustomize_${version}_${os}_${arch}.zip" output/$binary_name
-      else
-        tar cvfz "${release_dir}/kustomize_${version}_${os}_${arch}.tar.gz" -C output $binary_name
-      fi
-      rm output/$binary_name
-    done
-  done
-
-  # create checksums.txt
-  pushd "${release_dir}"
-  for release in *; do
-    echo "generate checksum: $release"
-    sha256sum "$release" >> checksums.txt
-  done
-  popd
-
-  rmdir output
-}
-
 function create_release {
 
   # Take everything before the last slash.
@@ -97,34 +49,8 @@ function create_release {
   git commit -a -m "create release branch $release_branch" || true
   git push -f origin $release_branch
 
-  additional_release_artifacts_arg=""
-
   # Trigger workflow for respective modeule release
   gh workflow run "release-${module_slugified}.yml" -f "release_type=${release_type}" -f "release_branch=${release_branch}"
-
-  # build `kustomize` binary
-  if [[ "$module" == "kustomize" ]]; then
-    release_artifact_dir=$(mktemp -d)
-    build_kustomize_binary "$nextVersion" "$release_artifact_dir"
-
-    # additional_release_artifacts_arg+="$release_artifact_dir/*"
-    additional_release_artifacts_arg=("$release_artifact_dir"/*)
-
-    # create github releases
-    gh release create "$git_tag" \
-      --title "$git_tag"\
-      --draft \
-      --notes-file "$changelog_file"\
-      "${additional_release_artifacts_arg[@]}"
-
-    return
-  fi
-
-  # create github releases
-  gh release create "$git_tag" \
-    --title "$git_tag"\
-    --draft \
-    --notes-file "$changelog_file"
 }
 
 function determineNextVersion {
