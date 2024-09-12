4034e36ee1bc1a1aee587c8a508fe4619fa1037d
Isar_NS <isar.nasimov98@gmail.com>
2024-09-13 01:43:12 +0300

    Add `--helm-debug` Flag to Kustomize for Enhanced Helm Debugging (#5751)
    * feat: add helm-debug flag
    
    * revert: go.work.sum
    
    * test: add helm chart args helm-debug test
    
    * test: helm debug flag
    
    * refactor: helm debug output
    
    * style: linting
    
    * revert: go.work.sum

--- api/types/helmchartargs_test.go
+++ api/types/helmchartargs_test.go
@@ -60,4 +60,21 @@ func TestAsHelmArgs(t *testing.T) {
 				"-f", "values1", "-f", "values2",
 				"--api-versions", "foo", "--api-versions", "bar"})
 	})
+
+	t.Run("use helm-debug", func(t *testing.T) {
+		p := types.HelmChart{
+			Name:                  "chart-name",
+			Version:               "1.0.0",
+			Repo:                  "https://helm.releases.hashicorp.com",
+			ValuesFile:            "values",
+			AdditionalValuesFiles: []string{"values1", "values2"},
+			Debug:                 true,
+		}
+		require.Equal(t, p.AsHelmArgs("/home/charts"),
+			[]string{"template", "--generate-name", "/home/charts/chart-name",
+				"-f", "values",
+				"-f", "values1",
+				"-f", "values2",
+				"--debug"})
+	})
 }
