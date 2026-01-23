9f2745ab5d01db1a0f0fcd61d1967cc01baaf327
David Lynch <davite3@protonmail.com>
2026-01-23 13:50:19 +0000

    feat: adding multiple runtimes for krm function

--- kyaml/fn/runtime/container/container.go
+++ kyaml/fn/runtime/container/container.go
@@ -6,6 +6,7 @@ package container
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"sigs.k8s.io/kustomize/kyaml/errors"
@@ -27,57 +28,62 @@ import (
 // Filter applies the function only to Resources to which it is scoped.
 //
 // Resources are scoped to a function if any of the following are true:
-// - the Resource were read from the same directory as the function config
-// - the Resource were read from a subdirectory of the function config directory
-// - the function config is in a directory named "functions" and
-//   they were read from a subdirectory of "functions" parent
-// - the function config doesn't have a path annotation (considered globally scoped)
-// - the Filter has GlobalScope == true
+//   - the Resource were read from the same directory as the function config
+//   - the Resource were read from a subdirectory of the function config directory
+//   - the function config is in a directory named "functions" and
+//     they were read from a subdirectory of "functions" parent
+//   - the function config doesn't have a path annotation (considered globally scoped)
+//   - the Filter has GlobalScope == true
 //
 // In Scope Examples:
 //
 // Example 1: deployment.yaml and service.yaml in function.yaml scope
-//            same directory as the function config directory
-//     .
-//     ├── function.yaml
-//     ├── deployment.yaml
-//     └── service.yaml
+//
+//	       same directory as the function config directory
+//	.
+//	├── function.yaml
+//	├── deployment.yaml
+//	└── service.yaml
 //
 // Example 2: apps/deployment.yaml and apps/service.yaml in function.yaml scope
-//            subdirectory of the function config directory
-//     .
-//     ├── function.yaml
-//     └── apps
-//         ├── deployment.yaml
-//         └── service.yaml
+//
+//	       subdirectory of the function config directory
+//	.
+//	├── function.yaml
+//	└── apps
+//	    ├── deployment.yaml
+//	    └── service.yaml
 //
 // Example 3: apps/deployment.yaml and apps/service.yaml in functions/function.yaml scope
-//            function config is in a directory named "functions"
-//     .
-//     ├── functions
-//     │   └── function.yaml
-//     └── apps
-//         ├── deployment.yaml
-//         └── service.yaml
+//
+//	       function config is in a directory named "functions"
+//	.
+//	├── functions
+//	│   └── function.yaml
+//	└── apps
+//	    ├── deployment.yaml
+//	    └── service.yaml
 //
 // Out of Scope Examples:
 //
 // Example 1: apps/deployment.yaml and apps/service.yaml NOT in stuff/function.yaml scope
-//     .
-//     ├── stuff
-//     │   └── function.yaml
-//     └── apps
-//         ├── deployment.yaml
-//         └── service.yaml
+//
+//	.
+//	├── stuff
+//	│   └── function.yaml
+//	└── apps
+//	    ├── deployment.yaml
+//	    └── service.yaml
 //
 // Example 2: apps/deployment.yaml and apps/service.yaml NOT in stuff/functions/function.yaml scope
-//     .
-//     ├── stuff
-//     │   └── functions
-//     │       └── function.yaml
-//     └── apps
-//         ├── deployment.yaml
-//         └── service.yaml
+//
+//	   .
+//	   ├── stuff
+//	   │   └── functions
+//	   │       └── function.yaml
+//	   └── apps
+//	       ├── deployment.yaml
+//	       └── service.yaml
 //
 // Default Paths:
 // Resources emitted by functions will have default path applied as annotations
@@ -86,42 +92,45 @@ import (
 // + function-file-name/ + namespace/ + kind_name.yaml
 //
 // Example 1: Given a function in fn.yaml that produces a Deployment name foo and a Service named bar
-//     dir
-//     └── fn.yaml
+//
+//	dir
+//	└── fn.yaml
 //
 // Would default newly generated Resources to:
 //
-//     dir
-//     ├── fn.yaml
-//     └── fn
-//         ├── deployment_foo.yaml
-//         └── service_bar.yaml
+//	dir
+//	├── fn.yaml
+//	└── fn
+//	    ├── deployment_foo.yaml
+//	    └── service_bar.yaml
 //
 // Example 2: Given a function in functions/fn.yaml that produces a Deployment name foo and a Service named bar
-//     dir
-//     └── fn.yaml
+//
+//	dir
+//	└── fn.yaml
 //
 // Would default newly generated Resources to:
 //
-//     dir
-//     ├── functions
-//     │   └── fn.yaml
-//     └── fn
-//         ├── deployment_foo.yaml
-//         └── service_bar.yaml
+//	dir
+//	├── functions
+//	│   └── fn.yaml
+//	└── fn
+//	    ├── deployment_foo.yaml
+//	    └── service_bar.yaml
 //
 // Example 3: Given a function in fn.yaml that produces a Deployment name foo, namespace baz and a Service named bar namespace baz
-//     dir
-//     └── fn.yaml
+//
+//	dir
+//	└── fn.yaml
 //
 // Would default newly generated Resources to:
 //
-//     dir
-//     ├── fn.yaml
-//     └── fn
-//         └── baz
-//             ├── deployment_foo.yaml
-//             └── service_bar.yaml
+//	dir
+//	├── fn.yaml
+//	└── fn
+//	    └── baz
+//	        ├── deployment_foo.yaml
+//	        └── service_bar.yaml
 type Filter struct {
 	runtimeutil.ContainerSpec `json:",inline" yaml:",inline"`
 
@@ -167,8 +176,24 @@ func (c *Filter) setupExec() error {
 	return nil
 }
 
+var runtimes []string = []string{
+	"docker",
+	"podman",
+	"crictl",
+	"nerdctl",
+}
+
 // getCommand returns the command + args to run to spawn the container
 func (c *Filter) getCommand() (string, []string) {
+
+	runtime := "docker"
+	for _, r := range runtimes {
+		if _, err := exec.LookPath(r); err == nil {
+			runtime = r
+			break
+		}
+	}
+
 	network := runtimeutil.NetworkNameNone
 	if c.ContainerSpec.Network {
 		network = runtimeutil.NetworkNameHost
@@ -197,7 +222,7 @@ func (c *Filter) getCommand() (string, []string) {
 
 	args = append(args, runtimeutil.NewContainerEnvFromStringSlice(c.Env).GetDockerFlags()...)
 	a := append(args, c.Image) //nolint:gocritic
-	return "docker", a
+	return runtime, a
 }
 
 // NewContainer returns a new container filter
