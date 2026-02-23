d05f3ce91c3f86cf112b1f7032a468b31f52ae9b
Stephen A. Imhoff <clockwork-muse@outlook.com>
2026-02-23 04:55:58 +0000

    fix: add initial push stub

--- api/internal/oci/pusher.go
+++ api/internal/oci/pusher.go
@@ -2,3 +2,125 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package oci
+
+import (
+	"github.com/distribution/reference"
+	"sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kyaml/errors"
+	"sigs.k8s.io/kustomize/kyaml/filesys"
+)
+
+type PushOptions struct {
+	fSys          *filesys.FileSystem
+	kustomization *types.Kustomization
+	targets       []reference.NamedTagged
+	annotations   map[string]string
+}
+
+func PushToOciRegistries(options *PushOptions) error {
+	// ctx := context.Background()
+
+	if len(options.targets) == 0 {
+		return errors.Errorf("At least one target is required.")
+	}
+
+	options.kustomization.FixKustomization()
+
+	// var dir string = filepath.Dir(mf.GetPath())
+
+	// fs, err := file.New("")
+	// if err != nil {
+	// 	return err
+	// }
+	// defer fs.Close()
+
+	// fileDescriptor, err := fs.Add(ctx, ".", "", dir)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// d := memory.New()
+
+	// oras.PackManifest(ctx, d, oras.PackManifestVersion1_1, "sdfsdf", oras.PackManifestOptions{})
+
+	// f, err := fSys.Open("")
+	// defer f.Close()
+
+	// d.Push(ctx, fileDescriptor, f)
+
+	// opts := oras.PackManifestOptions{
+	// 	Layers: []v1.Descriptor{
+	// 		fileDescriptor,
+	// 	},
+	// 	ManifestAnnotations: map[string]string{
+	// 		"org.opencontainers.image.created": o.createdAt.Format(time.RFC3339),
+	// 	},
+	// }
+
+	// artifactType := fmt.Sprintf("application/vnd.%s+%s", strings.ToLower(strings.ReplaceAll(kustomization.APIVersion, "/", ".")), strings.ToLower(kustomization.Kind))
+	// manifestDescriptor, err := oras.PackManifest(ctx, fs, oras.PackManifestVersion1_1, artifactType, opts)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// tag := "latest"
+	// if err = fs.Tag(ctx, manifestDescriptor, tag); err != nil {
+	// 	return err
+	// }
+
+	// dst, err := oci.New(o.registry)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// // }
+	// // reg, err := remote.NewRegistry(o.registry)
+	// // reg.PlainHTTP = true
+
+	// // dst, err := reg.Repository(ctx, "destination")
+	// // if err != nil {
+	// // 	panic(err) // Handle error
+	// // }
+
+	// desc, err := oras.Copy(ctx, fs, tag, dst, tag, oras.DefaultCopyOptions)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// log.Printf(`SUCCESS: published %s:%s@%s\n`, o.registry, tag, desc.Digest)
+
+	// return nil
+
+	// src, err := oci.New(repoSpec.RepoPath)
+	// if err != nil {
+	// 	return err
+	// }
+	// dir, err := filesys.NewTmpConfirmedDir()
+	// if err != nil {
+	// 	return err
+	// }
+
+	// repoSpec.Dir = dir
+
+	// fs, err := file.New(dir.String())
+	// if err != nil {
+	// 	return err
+	// }
+	// defer fs.Close()
+
+	// reference := "latest"
+	// if repoSpec.Tag != "" {
+	// 	reference = repoSpec.Tag
+	// } else if repoSpec.Digest != "" {
+	// 	reference = repoSpec.Digest
+	// }
+
+	// desc, err := oras.Copy(ctx, src, reference, fs, "", oras.DefaultCopyOptions)
+	// if err != nil {
+	// 	return err
+	// } else if repoSpec.Digest != "" && repoSpec.Digest != desc.Digest.String() {
+	// 	return errors.Errorf("expected digest %s, but pulled artifact with digest %s", repoSpec.Digest, desc.Digest)
+	// }
+
+	return nil
+}
