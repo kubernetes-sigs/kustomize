8f961d46629bd7842f66e6794a9daa534cac47b0
Yugo Kobayashi <kobdotsh@gmail.com>
2026-09-17 19:18:31 +0900

    refactor: share prefix and suffix field traversal

--- api/filters/suffix/suffix.go
+++ api/filters/suffix/suffix.go
@@ -4,10 +4,8 @@
 package suffix
 
 import (
-	"fmt"
-
-	"sigs.k8s.io/kustomize/api/filters/fieldspec"
 	"sigs.k8s.io/kustomize/api/filters/filtersutil"
+	"sigs.k8s.io/kustomize/api/filters/internal/affix"
 	"sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/kustomize/kyaml/kio"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
@@ -31,20 +29,5 @@ func (f *Filter) WithMutationTracker(callback func(key, value, tag string, node 
 }
 
 func (f Filter) Filter(nodes []*yaml.RNode) ([]*yaml.RNode, error) {
-	return kio.FilterAll(yaml.FilterFunc(f.run)).Filter(nodes)
-}
-
-func (f Filter) run(node *yaml.RNode) (*yaml.RNode, error) {
-	err := node.PipeE(fieldspec.Filter{
-		FieldSpec:  f.FieldSpec,
-		SetValue:   f.evaluateField,
-		CreateKind: yaml.ScalarNode, // Name is a ScalarNode
-		CreateTag:  yaml.NodeTagString,
-	})
-	return node, err
-}
-
-func (f Filter) evaluateField(node *yaml.RNode) error {
-	return f.trackableSetter.SetScalar(fmt.Sprintf(
-		"%s%s", node.YNode().Value, f.Suffix))(node)
+	return affix.Apply(nodes, f.FieldSpec, "", f.Suffix, f.trackableSetter) //nolint:wrapcheck // Preserve the filter's existing error chain.
 }
